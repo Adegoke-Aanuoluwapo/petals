39a5e0061360a027fcde5f3b6decd8ffd9ebd413
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-10 12:33:14 +0100

    finally

--- LoanCalculator/js/main.js
+++ LoanCalculator/js/main.js
@@ -1,3 +1,43 @@
+// const loanAmountInput = document.querySelector(".loan-amount");
+// const interestRateInput = document.querySelector(".interest-rate");
+// const loanTenureInput = document.querySelector(".loan-tenure");
+
+// const loanEMIValue = document.querySelector(".loan-emi .value");
+// const totalInterestValue = document.querySelector(".total-interest .value");
+// const totalAmountValue = document.querySelector(".total-amount .value");
+// const calculateBtn = document.querySelector(".calculate-btn");
+
+// let loanAmount = parseFloat(loanAmountInput.value);
+// let interestRate = parseFloat(interestRateInput.value);
+// let loanTenure = parseFloat(loanTenureInput.value);
+// let interest = interestRate / 12 / 100;
+
+// const calculateEMI = () => {
+//   let emi =
+//     loanAmount *
+//     interest *
+//     (Math.pow(1 + interest, loanTenure) /
+//       (Math.pow(1 + interest, loanTenure) - 1));
+
+//   return emi;
+// };
+
+// const updateData = (emi) => {
+//   loanEMIValue.innerHTML = Math.round(emi);
+//   let totalAmount = Math.round(loanTenure * emi);
+//   totalAmountValue.innerHTML = totalAmount;
+
+//   let totalInterestPayable = Math.round(totalAmount - loanAmount);
+//   totalAmountValue.innerHTML = totalInterestPayable;
+// };
+
+// const init = () => {
+//   let emi = calculateEMI();
+//   updateData(emi);
+// };
+
+// init();
+
 const loanAmountInput = document.querySelector(".loan-amount");
 const interestRateInput = document.querySelector(".interest-rate");
 const loanTenureInput = document.querySelector(".loan-tenure");
@@ -16,10 +56,8 @@ const calculateEMI = () => {
   let emi =
     loanAmount *
     interest *
-    interest(
-      Math.pow(1 + interest, loanTenure) /
-        (Math.pow(1 + interest, loanTenure) - 1)
-    );
+    (Math.pow(1 + interest, loanTenure) /
+      (Math.pow(1 + interest, loanTenure) - 1));
   return emi;
 };
 const updateData = (emi) => {
@@ -29,21 +67,19 @@ const updateData = (emi) => {
   let totalInterestPayable = Math.round(totalAmount - loanAmount);
   totalInterestValue.innerHTML = totalInterestPayable;
 };
-const init = () => {
-  let emi = calculateEMI();
-  updateData(emi);
-};
-
-init();
-
-const refreshInValues = () => {
+const refreshInputValues = () => {
   loanAmount = parseFloat(loanAmountInput.value);
   interestRate = parseFloat(interestRateInput.value);
   loanTenure = parseFloat(loanTenureInput.value);
   interest = interestRate / 12 / 100;
 };
 
-calculateBtn.addEventListener("click", () => {
+const init = () => {
+  refreshInputValues();
   let emi = calculateEMI();
   updateData(emi);
-});
+};
+
+init();
+
+calculateBtn.addEventListener("click", init);
