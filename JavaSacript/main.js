7d18f55b70346b0e69b1196c24bd121d01ae6e32
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-14 15:06:11 +0100

    close

--- JavaSacript/main.js
+++ JavaSacript/main.js
@@ -7,6 +7,38 @@
 //   firstName + " " + lastName,
 //   "and your age is " + age
 // );
-firstName = prompt("Please enter your firstName");
-age = prompt("Please enter your age");
-alert(firstName + "how are you, I could see you are " + age + "years old");
+// firstName = prompt("Please enter your firstName");
+// age = prompt("Please enter your age");
+// alert(firstName + "how are you, I could see you are " + age + "years old");
+// let person = {
+//   firstName: "John",
+//   lastName: "Doe",
+// };
+// console.log(person.firstName);
+// let address = {
+//   "building no": 3960,
+//   street: "North 1st street",
+//   state: "CA",
+//   country: "USA",
+// };
+// console.log(address["building no"]);
+// console.log(address.state);
+// console.log(address["street"]);
+// let person = {
+//   name: "John",
+//   age: 25,
+// };
+// person.ssn = "123-45-6789";
+// person.name = "John Doe";
+// person.complexion = "black";
+let person = {
+  name: "John",
+  age: 25,
+};
+
+let member = person;
+
+member.age = 26;
+
+console.log(person);
+console.log(member);
