fc6b3e48d27b7d30453b731482370c5d6483f551
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-14 14:07:20 +0100

    restarting

---
+++ JavaSacript/main.js
@@ -0,0 +1,12 @@
+// var firstName = "John";
+// console.log(firstName);
+// var lastName = "Olatunde";
+// var age = 28;
+// console.log(
+//   "Your name is ",
+//   firstName + " " + lastName,
+//   "and your age is " + age
+// );
+firstName = prompt("Please enter your firstName");
+age = prompt("Please enter your age");
+alert(firstName + "how are you, I could see you are " + age + "years old");
