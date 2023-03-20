6a3523117baa8644bf3c54c283371b0e5f7a239d
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-20 12:09:02 +0100

    prototype

--- JavaSacript/main.js
+++ JavaSacript/main.js
@@ -362,35 +362,65 @@
 // let person = new Person("John", "Doe");
 // console.log(person.getFullName());
 
-function calculateTips(bill, tip) {
-  if (bill > 0 && bill < 50) {
-    tip = 0.2 * bill;
-    return tip;
-  } else if (bill >= 50 && bill < 200) {
-    tip = 0.15 * bill;
-    return tip;
-  } else if (bill > 200) {
-    tip = 0.1 * bill;
-    return tip;
-  }
-}
-console.log(calculateTips(48));
-console.log(calculateTips(124));
-console.log(calculateTips(268));
+// function calculateTips(bill, tip) {
+//   if (bill > 0 && bill < 50) {
+//     tip = 0.2 * bill;
+//     return tip;
+//   } else if (bill >= 50 && bill < 200) {
+//     tip = 0.15 * bill;
+//     return tip;
+//   } else if (bill > 200) {
+//     tip = 0.1 * bill;
+//     return tip;
+//   }
+// }
+// console.log(calculateTips(48));
+// console.log(calculateTips(124));
+// console.log(calculateTips(268));
 
-const tip = [];
-tip[0] = calculateTips(48);
-tip[1] = calculateTips(124);
-tip[2] = calculateTips(268);
-console.log("The tips paid John's family are:");
-console.log(tip);
-const bill = [];
-bill[0] = 124;
-bill[1] = 48;
-bill[2] = 268;
-console.log("The final amount paid John's family are:");
-const amt = [];
-amt[0] = tip[0] + bill[0];
-amt[1] = tip[1] + bill[1];
-amt[2] = tip[2] + bill[2];
-console.log(amt);
+// const tip = [];
+// tip[0] = calculateTips(48);
+// tip[1] = calculateTips(124);
+// tip[2] = calculateTips(268);
+// console.log("The tips paid John's family are:");
+// console.log(tip);
+// const bill = [];
+// bill[0] = 124;
+// bill[1] = 48;
+// bill[2] = 268;
+// console.log("The final amount paid John's family are:");
+// const amt = [];
+// amt[0] = tip[0] + bill[0];
+// amt[1] = tip[1] + bill[1];
+// amt[2] = tip[2] + bill[2];
+// console.log(amt);
+// function Person(firstName, lastName) {
+//   console.log(new.target);
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+
+//   this.getFullName = function () {
+//     return this.firstName + " " + this.lastName;
+//   };
+// }
+// let person = new Person("John", "Doe");
+// function Person(firstName, lastName) {
+//   if (!new.target) {
+//     throw Error("Cannot be called without the new keyword");
+//   }
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+// }
+// Person();
+let person = { name: "John" };
+
+console.log(person);
+console.log(person.name);
+console.log(person.toString());
+console.log(Object.prototype);
+console.log(typeof Object);
+function Person(name) {
+  this.name = name;
+}
+console.log(Person);
+console.log(Person.prototype);
