0ae63aeb3d30773b2b6060defc64283e8017a731
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-20 11:07:18 +0100

    progress

--- JavaSacript/main.js
+++ JavaSacript/main.js
@@ -315,20 +315,20 @@
 // function bill(){
 
 // }
-let taxRate = () => 0.1;
-let getPrice = function (price, tax = price * taxRate()) {
-  return price + tax;
-};
-let fullPrice = getPrice(100);
-console.log(fullPrice);
+// let taxRate = () => 0.1;
+// let getPrice = function (price, tax = price * taxRate()) {
+//   return price + tax;
+// };
+// let fullPrice = getPrice(100);
+// console.log(fullPrice);
 
-function add(x, y = 1, z = 2) {
-  console.log(arguments.length);
-  return x + y + z;
-}
-add(10, 23, 10, 30);
-add(10, 20);
-add(10, 20, 30);
+// function add(x, y = 1, z = 2) {
+//   console.log(arguments.length);
+//   return x + y + z;
+// }
+// add(10, 23, 10, 30);
+// add(10, 20);
+// add(10, 20, 30);
 
 // let person = {
 //   firstName: "John",
@@ -361,8 +361,36 @@ add(10, 20, 30);
 // }
 // let person = new Person("John", "Doe");
 // console.log(person.getFullName());
+
+function calculateTips(bill, tip) {
+  if (bill > 0 && bill < 50) {
+    tip = 0.2 * bill;
+    return tip;
+  } else if (bill >= 50 && bill < 200) {
+    tip = 0.15 * bill;
+    return tip;
+  } else if (bill > 200) {
+    tip = 0.1 * bill;
+    return tip;
+  }
+}
+console.log(calculateTips(48));
+console.log(calculateTips(124));
+console.log(calculateTips(268));
+
+const tip = [];
+tip[0] = calculateTips(48);
+tip[1] = calculateTips(124);
+tip[2] = calculateTips(268);
+console.log("The tips paid John's family are:");
+console.log(tip);
 const bill = [];
-bill[0] = 45;
-bill[1] = 60;
-bill[2] = 100;
-console.log(bill);
+bill[0] = 124;
+bill[1] = 48;
+bill[2] = 268;
+console.log("The final amount paid John's family are:");
+const amt = [];
+amt[0] = tip[0] + bill[0];
+amt[1] = tip[1] + bill[1];
+amt[2] = tip[2] + bill[2];
+console.log(amt);
