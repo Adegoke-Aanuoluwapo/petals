bb7267649b87712562f385c5b6be33e74fdac178
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-21 09:47:04 +0100

    singler

--- JavaSacript/main.js
+++ JavaSacript/main.js
@@ -600,4 +600,9 @@ class Person {
     this.firstName = firstName;
     this.lastName = lastName;
   }
+  get [name]() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
+let person = new Person("John", "Doe");
+console.log(person.fullName);
