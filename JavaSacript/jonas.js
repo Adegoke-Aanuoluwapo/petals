3bc23f7760dc7bfc713106cf672f22e4640c885d
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-22 11:06:51 +0100

    retirement

--- JavaSacript/jonas.js
+++ JavaSacript/jonas.js
@@ -2,3 +2,9 @@ function calculateAge(year) {
   return 2023 - year;
 }
 console.log(calculateAge(1963));
+function retirement(year, firstName) {
+  var age = calculateAge(year);
+  var retire = 65 - age;
+  console.log(firstName + " retires in " + retire + " years");
+}
+retirement(1970, "Ademefun");
