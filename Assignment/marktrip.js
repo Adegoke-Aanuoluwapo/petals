90b63ce6dab48dc7a0da05546ad14fefd931aaf8
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-22 09:20:08 +0100

    percent

--- Assignment/marktrip.js
+++ Assignment/marktrip.js
@@ -4,18 +4,18 @@ function tipping(bill, tip) {
   this.bill = [];
   this.tip = [];
   this.percent = percent;
-  this.bills.array.forEach(element => {
-   
+  this.bills.forEach((bill, index) => {
+    if (bill < 100) {
+      percent = 0.2;
+      tip = percent * bill;
+    } else if (bill >= 100 && bill < 300) {
+      percent = 0.1;
+      tip = percent * bill;
+    } else if (bill > 300) {
+      percent = 0.25;
+      tip = percent * bill;
+    }
+    this.tip[index] = percent * bill;
+    this.finalBill[index] = bill + percent * bill;
   });
-  if (bill < 100) {
-    percent = 0.2;
-    tip = percent * bill;
-  }
-  else if(bill >= 100 && bill < 300){
-   percent = 0.10
-   tip = percent *bill
-  }else(bill > 300){
-   percent = 0.25
-   tip = percent * bill
-  }
 }
