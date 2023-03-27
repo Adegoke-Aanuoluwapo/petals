deb0d6c694209b7e08c0d1a33d9eaab0f2428471
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-27 11:34:34 +0100

    comb

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,3 +5,8 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('#current--' + activePlayer).textContent = dice;
+// document.querySelector('#current--1').innerHTML = dice;
+// console.log(dice);
+var x = document.querySelector('#score--0').textContent;
+console.log(x);
+document.querySelector('.dice').style.display = 'none';
