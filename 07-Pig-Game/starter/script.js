906d42eb006d67e8a6110267cd0885cb68aa9557
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-27 12:54:47 +0100

    reset

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,16 +2,28 @@ var scores, roundScores, activePlayer, dice;
 score = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current--' + activePlayer).textContent = dice;
-// document.querySelector('#current--1').innerHTML = dice;
-// console.log(dice);
-var x = document.querySelector('#score--0').textContent;
-console.log(x);
 document.querySelector('.dice').style.display = ' none';
+document.getElementById('score--0').textContent = '0';
+document.getElementById('score--1').textContent = '0';
+document.getElementById('current--0').textContent = '0';
+document.getElementById('current--1').textContent = '0';
 function btn() {
   console.log('button pressed');
 }
 btn();
-document.querySelector('btn--roll').addEventListener('click', btn);
+document.querySelector('.btn--roll').addEventListener('click', function () {
+  // console.log('button is pressed');
+  // Random number
+  dice = Math.floor(Math.random() * 6) + 1;
+  //Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+});
+
+//document.querySelector('#current--' + activePlayer).textContent = dice;
+// document.querySelector('#current--1').innerHTML = dice;
+// console.log(dice);
+//var x = document.querySelector('#score--0').textContent;
+//console.log(x);
