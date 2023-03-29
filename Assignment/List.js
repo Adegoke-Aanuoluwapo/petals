00edc568ca8db2fb86187d8ef38a1bf683559438
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-03-29 09:32:47 +0100

    child

--- Assignment/List.js
+++ Assignment/List.js
@@ -1,8 +1,13 @@
-let x = document.querySelectorAll("ul >li");
-x[0].innerHTML = "first";
-x[2].innerHTML = "last";
-x[3].innerHTML = "first";
-x[5].innerHTML = "last";
-x[6].innerHTML = "first";
-x[8].innerHTML = "last";
-console.log(x);
+let first = [...document.querySelectorAll("ul.list li:first-child")];
+let last = [...document.querySelectorAll(".list li:last-child")];
+
+first.forEach((el) => (el.innerHTML = "First"));
+last.forEach((el) => (el.innerHTML = "Last"));
+
+console.log(first, last);
+//items[].innerHTML = "first";
+// x[2].innerHTML = "last";
+// x[3].innerHTML = "first";
+// x[5].innerHTML = "last";
+// x[6].innerHTML = "first";
+// x[8].innerHTML = "last";
