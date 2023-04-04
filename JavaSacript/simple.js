05674464f40e116a90474dd6d2f1d144fe465363
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-04 09:12:02 +0100

    new day

--- JavaSacript/simple.js
+++ JavaSacript/simple.js
@@ -2,3 +2,5 @@ let note = document.getElementById("note");
 console.log(note.textContent);
 console.log(note.innerText);
 console.log("yes");
+node.textContent = newText;
+note.textContent = "This is a note";
