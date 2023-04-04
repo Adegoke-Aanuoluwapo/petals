7cd20ba8a8a1fa8313ccb56bf184723b48bed774
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-04 14:58:57 +0100

    attribute

--- Assignment/Selector.js
+++ Assignment/Selector.js
@@ -6,5 +6,12 @@ const third = document.querySelector("#third");
 console.log(third);
 const fourth = document.querySelector("#fourth");
 console.log(fourth);
-const par = document.querySelectorAll("p");
+const par = [...document.querySelectorAll("p")];
 console.log(par);
+fourth.textContent = "Fourth Paragraph";
+for (let con of par) {
+  console.log(`${par}= ${par.textContent}`);
+}
+//Setting an attribute
+first.setAttribute("secured", "true");
+second.setAttribute("secured", "false");
