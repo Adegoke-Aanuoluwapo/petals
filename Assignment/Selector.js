8350173da433a3415149b0e3194aaf435740694d
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-04 13:34:00 +0100

    id

---
+++ Assignment/Selector.js
@@ -0,0 +1,9 @@
+const first = document.querySelector("#first");
+console.log(first);
+const second = document.querySelector("#second");
+console.log(second);
+const third = document.querySelector("#third");
+console.log(third);
+const fourth = document.querySelector("#fourth");
+console.log(fourth);
+const par = document.querySelectorAll();
