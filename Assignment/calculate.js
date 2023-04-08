869eef4ff92e3676cd44dfd5933787a8d3bad8be
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-08 23:20:25 +0100

    hello

--- Assignment/calculate.js
+++ Assignment/calculate.js
@@ -1,6 +1,8 @@
 let square = document.querySelector("#square-button");
-function squared(n) {
-  console.log("Hello");
+function squared(x) {
+  console.log(x * x);
 }
 let input = document.querySelector("#square-input");
 console.log(input);
+x =input.innerHTML
+console.log(x)
