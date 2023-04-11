1240e92f6105dafc1e7c9beccd25c16f257336d8
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-11 13:31:19 +0100

    grit

--- Assignment/calculate.js
+++ Assignment/calculate.js
@@ -1,9 +1,20 @@
-let input = document.getElementById("square-input");
-console.log(input);
-x = input.value;
-console.log(x);
+function Square() {
+  val = document.getElementById("square-input").value;
+  square = Number(val) * Number(val);
 
-let square = document.querySelector("#square-button");
-function squared(x) {
-  console.log(x * x);
+  output = document.getElementById("square-output");
+  output.value = square;
+}
+btn1 = document.getElementById("square-button");
+btn1.addEventListener("click", Square);
+function Half() {
+  ave = parseFloat(document.getElementById("half-input").value);
+  half = ave / 2;
+  halfOutput = document.getElementById("half-output").value;
+  halfOutput = half;
+}
+btn2 = document.getElementById("half-button");
+btn2.addEventListener("click", Half);
+function Area() {
+  num = document.getElementById("");
 }
