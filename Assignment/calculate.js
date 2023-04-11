9f59c40741385a2af968ebec8b178fff383c186a
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-11 14:02:14 +0100

    whole

--- Assignment/calculate.js
+++ Assignment/calculate.js
@@ -8,13 +8,22 @@ function Square() {
 btn1 = document.getElementById("square-button");
 btn1.addEventListener("click", Square);
 function Half() {
-  ave = parseFloat(document.getElementById("half-input").value);
-  half = ave / 2;
-  halfOutput = document.getElementById("half-output").value;
-  halfOutput = half;
+  valu = document.getElementById("half-input").value;
+  half = parseFloat(valu) / 2;
+  halfOutput = document.getElementById("half-output");
+  halfOutput.value = half;
 }
 btn2 = document.getElementById("half-button");
 btn2.addEventListener("click", Half);
 function Area() {
-  num = document.getElementById("");
+  num = document.getElementById("area-input").value;
+  area = (Number(num) * 22) / 7;
+  areaOut = document.getElementById("area-output");
+  areaOut.value = area;
+}
+btn4 = document.getElementById("area-button");
+btn4.addEventListener("click", Area);
+function Fraction() {
+  frac = document.getElementById("percent1-input").value;
+  fraction = parseFloat(frac) / 100;
 }
