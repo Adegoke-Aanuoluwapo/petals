9c65602173bb4401dc1daaaf1a2b000560ed7a0a
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-11 09:57:28 +0100

    done

--- JavaSacript/input.js
+++ JavaSacript/input.js
@@ -4,8 +4,9 @@ let output = document.querySelector("#tbuser");
 console.log(output);
 let button = document.getElementById("btn1");
 console.log(btn1);
-let answer = input.value;
-function display(answer) {
-  console.log(answer);
+const out1 = document.getElementById("output1");
+
+function fun1() {
+  out1.innerHTML = input.value;
 }
-let par = document.getElementById("output1");
+btn1.addEventListener("click", fun1);
