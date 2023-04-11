2ff772d6e82a2ef14bfc89d74fac85cb5bbd836c
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-11 09:43:39 +0100

    advance

--- JavaSacript/input.js
+++ JavaSacript/input.js
@@ -2,3 +2,10 @@ let input = document.getElementById("tbuser");
 console.log(input);
 let output = document.querySelector("#tbuser");
 console.log(output);
+let button = document.getElementById("btn1");
+console.log(btn1);
+let answer = input.value;
+function display(answer) {
+  console.log(answer);
+}
+let par = document.getElementById("output1");
