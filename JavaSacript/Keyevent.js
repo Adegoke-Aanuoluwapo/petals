af1bf45498e1c124932508df1c9aabdd0db727bd
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-12 14:59:40 +0100

    todo

--- JavaSacript/Keyevent.js
+++ JavaSacript/Keyevent.js
@@ -23,3 +23,12 @@ function showMessage(input, message, type) {
 function showError(input, message) {
   return showMessage(input, message, false);
 }
+function showSuccess(input) {
+  return showMessage(input, message, true);
+}
+function hasValue(input, message) {
+  if (input.value.trim() === "") {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
