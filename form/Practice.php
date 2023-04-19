93469613a20fe23c32d0ffc136d4dacb5661e25c
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-19 14:17:28 +0100

    practice

---
+++ form/Practice.php
@@ -0,0 +1,24 @@
+<?php
+
+$email = null;
+var_dump($email);
+$name = "Edward";
+var_dump($name);
+echo $name
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
