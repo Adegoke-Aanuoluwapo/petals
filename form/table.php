3726fb403e232b460abcd957658b26904770a200
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-19 11:54:14 +0100

    done

--- form/table.php
+++ form/table.php
@@ -9,7 +9,7 @@ if ($_POST) {
  $school = $_POST['school'];
  $state = $_POST['state'];
  $LGA = $_POST['LGA'];
-}
+};
 ?>
 
 <!DOCTYPE html>
@@ -22,26 +22,26 @@ if ($_POST) {
 </head>
 
 <body>
- <form method="$_POST">
+ <form method='POST'>
   Surname:
-  <input type="text" value="surname"><br><br>
+  <input type="text" name="surname"><br><br>
   Firstname:
-  <input type="text" value="firstname"><br><br>
+  <input type="text" name="firstname"><br><br>
   Class:
-  <input type="text" value="class"><br><br>
+  <input type="text" name="class"><br><br>
   Date of Birth:
-  <input type="text" value="dateOfBirth"><br><br>
-  Number:
-  <input type="text" value="number"><br><br>
+  <input type="date" name="dateOfBirth"><br><br>
+  Phone Number:
+  <input type="number" name="number"><br><br>
   Address:
-  <input type="text" value="address"><br><br>
+  <input type="text" name="address"><br><br>
   School:
-  <input type="text" value="school"><br><br>
+  <input type="text" name="school"><br><br>
   State:
-  <input type="text" value="state"><br><br>
+  <input type="text" name="state"><br><br>
   LGA:
-  <input type="text" value="LGA">
-<input type="submit" name="submit">
+  <input type="text" name="LGA">
+  <input type="submit" name="submit">
  </form>
  <table>
   <thead>
@@ -53,39 +53,39 @@ if ($_POST) {
   <tbody>
    <tr>
     <td>Surname</td>
-    <td><input type="text" value="<?=$surname ?>"></td>
+    <td><input type="text" value="<?= $surname ?>"></td>
    </tr>
    <tr>
     <td>First Name</td>
-    <td><input type="text" value="<?=$firstname ?>"></td>
+    <td><input type="text" value="<?= $firstname ?>"></td>
    </tr>
    <tr>
     <td>Class</td>
-    <td><input type="text" value="<?=$class ?>"></td>
+    <td><input type="text" value="<?= $class ?>"></td>
    </tr>
    <tr>
     <td>Date of Birth</td>
-    <td><input type="text" value="<?=$dateOfBirth ?>"></td>
+    <td><input type="text" value="<?= $dateOfBirth ?>"></td>
    </tr>
    <tr>
     <td>Number</td>
-    <td><input type="text" value="<?=$number ?>"></td>
+    <td><input type="text" value="<?= $number ?>"></td>
    </tr>
    <tr>
     <td>Address</td>
-    <td><input type="text" value="<?=$address ?>"></td>
+    <td><input type="text" value="<?= $address ?>"></td>
    </tr>
    <tr>
     <td>School</td>
-    <td><input type="text" value="<?=$school ?>"></td>
+    <td><input type="text" value="<?= $school ?>"></td>
    </tr>
    <tr>
     <td>State</td>
-    <td><input type="text" value="<?=$state ?>"></td>
+    <td><input type="text" value="<?= $state ?>"></td>
    </tr>
    <tr>
     <td>LGA</td>
-    <td><input type="text" value="<?=$LGA ?>"></td>
+    <td><input type="text" value="<?= $LGA ?>"></td>
    </tr>
   </tbody>
  </table>
