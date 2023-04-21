2f405149aa8d1201457390f14742b3c96ae2487a
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-21 10:09:52 +0100

    hi

--- form/forStatement.php
+++ form/forStatement.php
@@ -1,11 +1,25 @@
+<?php $total = 0;
+$number =1;
+for ($i = 1; $i <= 10; $i++) {
+ 
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Document</title>
 </head>
+
 <body>
- 
+ <h1><?php
+     echo $total
+
+     ?></h1>
 </body>
+
 </html>
