67481838c2cc0515f2bc61082ea2691fe8eab20a
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-23 15:00:27 +0100

    fun

--- form/function.php
+++ form/function.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Document</title>
 </head>
+
 <body>
- <?php
- echo "You are Welcome";
+ <!-- <?php
+      echo "You are Welcome";
 
- function welcome(){
-  echo "You are welcome";
- }
- welcome();
+      function welcome()
+      {
+       echo "You are welcome";
+      }
+      welcome();
 
- function welcome_user($username){
-  return 'Welcome '. $username . "!";
- }
+      function welcome_user($username)
+      {
+       return 'Welcome ' . $username . "!";
+      }
 
- echo welcome_user("Ojindo")
+      echo welcome_user("Ojindo");
+      ?> -->
+ <?php function welcom_user($username)
+ { ?>
+  <span>welcome<?= $username ?></span>
+
+ <?php }
+ echo welcom_user(" Olugunade");
  ?>
- <?php function welcome_user($username) { ?>
-    <span>welcome<?=$username ?></span>
- <?php } ?>
+
 </body>
+
 </html>
