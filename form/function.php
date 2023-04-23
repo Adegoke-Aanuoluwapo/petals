68d5290c6a68aa54e7038ebc4d77ce6ab2d4df74
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-23 23:09:36 +0100

    counter

--- form/function.php
+++ form/function.php
@@ -39,6 +39,16 @@
 
  echo $greeting;
  ?>
+ <?php 
+ $counter = 1;
+ function increase($value){
+      $value+= 1;
+      echo $value. '<br>';
+ }
+ increase($counter);
+ echo $counter . "<br>";
+
+ ?>
 
 </body>
 
