c298ef2cbd2b478cfcb6faf48eaaefb4ac8cb922
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-25 09:38:58 +0100

    arrayfunc

--- form/arrays.php
+++ form/arrays.php
@@ -6,6 +6,19 @@
  <title>Document</title>
 </head>
 <body>
+ <h1><?php
+  $scores = [1, 2, 3, 5];
+ // echo '<pre>';
+ // print_r($scores);
+ // echo '<pre>';
+
+ function print_array($data){
+  echo '<pre>';
+  print_r($data);
+  echo '</pre>';
+ }
+ print_array($scores)
  
+ ?></h1>
 </body>
 </html>
