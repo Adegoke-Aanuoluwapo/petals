92a6190ee293b4bea5e3800b0c647e5a5516f3ee
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-25 09:16:18 +0100

    variadfunc

--- form/variadicfunc.php
+++ form/variadicfunc.php
@@ -6,6 +6,18 @@
  <title>Document</title>
 </head>
 <body>
+ <?php
+ function sum(){
+  $numbers = func_get_args();
+  $total = 0;
+  for ($i =0; $i < count($numbers); $i++){
+   $total += $numbers[$i];
+  }
+  return $total;
+
+ }
+ echo sum(39, 34, 20)
  
+ ?>
 </body>
 </html>
