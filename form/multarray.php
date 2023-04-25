10c83621f6a9b0aba38a5eade430277cd6231b98
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-25 13:48:31 +0100

    array

--- form/multarray.php
+++ form/multarray.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Document</title>
 </head>
+
 <body>
- 
+ <h1><?php
+     $tasks = [
+      ['Learning PHP programming', 2],
+      ['Practice PHP', 2],
+      ['Work', 8],
+      ['Do excercise', 1]
+     ];
+     print_r($tasks) . '<br>';
+     foreach($tasks as $task){
+       foreach($task as $details){
+        echo $details;
+       }
+       echo '<br>';
+     };
+     echo $tasks[0][0];
+
+     usort($tasks, function($a, $b){
+      return $a[1] <=> $b[1];
+     })
+
+     ?></h1>
 </body>
+
 </html>
