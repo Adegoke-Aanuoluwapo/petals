09b9c8e6635640dc888992e904508330cd3cb922
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-25 10:39:11 +0100

    multidimensional

--- form/arrays.php
+++ form/arrays.php
@@ -21,12 +21,33 @@
  echo $scores[0];
  $scores[4] = 34;
  print_array($scores);
- echo count($scores);
+ echo count($scores) .'<br>';
 
- $html['title'] = 'PHP Associative Arrays';
- $html['description'] = 'Learn how to use associative arays in PHP';
- print_r($html)
+ // $html['title'] = 'PHP Associative Arrays';
+ // $html['description'] = 'Learn how to use associative arays in PHP';
+ // print_r($html) .'<br>';
  
+
+ $colors = ['red', 'green', 'blue'];
+ foreach($colors as $color){
+  echo $color .'<br>';
+ }
+ echo '<pre>';
+ echo $color;
+ echo '<pre>';
+ $capitals = [
+  'Japan' => 'Tokyo',
+  'France' => 'Paris',
+  'Germany' => 'Berlin',
+  'United Kingdom' => 'London',
+  'United State' => 'Washington'
+ ];
+ foreach($capitals as $country=> $capital){
+  echo "The capital of {$country} is {$capital}" .'<br>';
+ };
+echo '<pre>';
+print_r ($capitals);
+echo '<pre>';
  ?></h1>
 </body>
 </html>
