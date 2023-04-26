6f45361871bec532739ba77a85a7a8f09dc673c5
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-26 09:32:32 +0100

    file handling

--- form/file_handling.php
+++ form/file_handling.php
@@ -4,16 +4,47 @@ $filename = 'user.txt';
 
 // if (file_exists($file)) {
 //  echo file_get_contents($file);
- // $handle = fopen($file, 'r');
- // $contents = fread($handle, filesize($file));
- // fclose($handle);
- // echo $contents;
+// $handle = fopen($file, 'r');
+// $contents = fread($handle, filesize($file));
+// fclose($handle);
+// echo $contents;
 //};
-echo "Welcome home";
+// echo "Welcome home";
 
+// if (file_exists($filename)){
+// echo "$filename was las accessed: ".date("F d Y H:i:s.",fileatime($filename));
+// }
+// $f = fopen($filename, 'r');
+// if ($f) {
+//  echo 'The file ' . $filename
+//   . 'is open';
+//  fclose($f);
+// }
+if(file_exists($filename)){
+ echo '<pre>';
+ echo file_get_contents($filename);
+ echo '</pre>';
+};
+echo '<br>';
+
+// $file2 ="../JavaScript/using.txt";
+// if(file_exists($file2)){
+//  echo file_get_contents($file2);
+//}
 if (file_exists($filename)){
-echo "$filename was las accessed: ".date("F d Y H:i:s.",fileatime($filename));
+ $message ="The file $filename exist";
+
+}else{
+ $message = "THe file $filename does not exist";
 }
+echo $message;
+if(is_file($filename)){
+ $message2 ="The file $filename exist";
+
+}else{
+ $message2 = "The file $filename does not exist";
+}
+echo $message2
 ?>
 <!DOCTYPE html>
 <html lang="en">
