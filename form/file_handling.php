7fb4b0d010293778d2bc3a019cb71aa9db5513ff
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-26 08:51:07 +0100

    opening file

--- form/file_handling.php
+++ form/file_handling.php
@@ -1,11 +1,30 @@
+<?php
+
+$file = 'user.txt';
+
+if (file_exists($file)) {
+ echo file_get_contents($file);
+ // $handle = fopen($file, 'r');
+ // $contents = fread($handle, filesize($file));
+ // fclose($handle);
+ // echo $contents;
+};
+echo "Welcome home";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Document</title>
 </head>
+
 <body>
- 
+ <?php
+
+ ?>
+
 </body>
+
 </html>
