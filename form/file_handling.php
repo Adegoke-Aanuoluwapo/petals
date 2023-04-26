f2f4374d9fbed6028ae6b9b6def816a76b630d2d
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-26 09:00:50 +0100

    timing

--- form/file_handling.php
+++ form/file_handling.php
@@ -1,15 +1,19 @@
 <?php
 
-$file = 'user.txt';
+$filename = 'user.txt';
 
-if (file_exists($file)) {
- echo file_get_contents($file);
+// if (file_exists($file)) {
+//  echo file_get_contents($file);
  // $handle = fopen($file, 'r');
  // $contents = fread($handle, filesize($file));
  // fclose($handle);
  // echo $contents;
-};
+//};
 echo "Welcome home";
+
+if (file_exists($filename)){
+echo "$filename was las accessed: ".date("F d Y H:i:s.",fileatime($filename));
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
