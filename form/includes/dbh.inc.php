d4c49893fe4abbbfabdeafa9e26701328bd037e4
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-27 15:24:14 +0100

    once

--- form/includes/dbh.inc.php
+++ form/includes/dbh.inc.php
@@ -4,7 +4,7 @@ $dbUsername = "root";
 $dbPassword ="";
 $dbName = "";
 
-$conn = msqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
 
 
 
