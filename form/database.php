8f3a207b7c53f7dbd7ae25fa28d792276c147d96
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-28 06:48:17 +0100

    start

--- form/database.php
+++ form/database.php
@@ -1,16 +1,19 @@
-<?php 
+<?php
 include_once 'includes/dbh.inc.php';
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Document</title>
 </head>
+
 <body>
- 
+ $conn;
 </body>
+
 </html>
