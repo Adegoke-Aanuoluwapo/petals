45eb246afe6f9ea6a3c91a730b18c37ffc0b20fa
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-04-29 12:55:24 +0100

    good

--- form/sorting.php
+++ form/sorting.php
@@ -1,32 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Document</title>
 </head>
+
 <body>
-<?php 
-$numbers = [2, 1, 3];
+ <?php
+ $numbers = [2, 1, 3];
 
-sort($numbers);
-print_r($numbers);
+ sort($numbers);
+ print_r($numbers);
 
-$names = ['Bob', 'John', 'Alice'];
-sort($names, SORT_STRING);
-print_r($names);
+ $names = ['Bob', 'John', 'Alice'];
+ sort($names, SORT_STRING);
+ print_r($names);
 
-$fruits = ['apple', 'Banana', 'orange'];
-sort($fruits, SORT_FLAG_CASE);
+ $fruits = ['apple', 'Banana', 'orange'];
+ sort($fruits, SORT_FLAG_CASE);
 
-print_r($fruits);
-$rank = ['A-1', 'A-2', 'A-12', 'A-11'];
-rsort($ranks, SORT_STRING | SORT_NATURAL);
-print_r($ranks);
+ // print_r($fruits);
+ // $rank = ['A-1', 'A-2', 'A-12', 'A-11'];
+ // rsort($ranks, SORT_STRING | SORT_NATURAL);
+ // print_r($ranks);
+ $employees = [
+  'John' => [
+   'age' => 24,
+   'title' => 'Front-end Developer'
+  ],
+  'alice' => [
+   'age' => 28,
+   'title' => 'Web Designer'
+  ],
+  'bob' => [
+   'age' => 25,
+   'title' => 'MySQL DBA'
+  ]
+ ];
+ ksort($employees, SORT_STRING);
+ print_r($employees)
 
 
+ ?>
 
-?>
- 
 </body>
+
 </html>
