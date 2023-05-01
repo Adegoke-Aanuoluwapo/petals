04f4bb21fe9795ae737518a32f7224d0f1e4ed54
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-01 13:36:55 +0100

    create

--- form/myshop/index.php
+++ form/myshop/index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Document</title>
+ <title>My Shop</title>
+ <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
- 
+ <div class="container my-5">
+  <h2>List of clients </h2>
+  <a class="btn btn-primary" href="/myshop/create.php" role="button">New Clients</a><br>
+ </div>
 </body>
+
 </html>
