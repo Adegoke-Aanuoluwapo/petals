47797143d4d70196fa2e95af279fc4267db383d5
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-02 13:37:15 +0100

    admin

--- form/myshop/create.php
+++ form/myshop/create.php
@@ -9,7 +9,41 @@
 </head>
 
 <body>
-
+ <div class="container my-5">
+  <h2>New Client</h2>
+  <form method="post">
+   <div class="row mb-3">
+    <label class="col-sm-3 col-form-label">Names</label>
+    <div class="col-sm-6">
+     <input type="text" class="form-content" name="name" value="">
+    </div>
+   </div>
+   <div class="row mb-3">
+    <label class="col-sm-3 col-form-label">Email</label>
+    <div class="col-sm-6">
+     <input type="text" class="form-content" name="name" value="">
+    </div>
+   </div>
+   <div class="row mb-3">
+    <label class="col-sm-3 col-form-label">Phones</label>
+    <div class="col-sm-6">
+     <input type="text" class="form-content" name="name" value="">
+    </div>
+   </div>
+   <div class="row mb-3">
+    <label class="col-sm-3 col-form-label">Address</label>
+    <div class="col-sm-6">
+     <input type="text" class="form-content" name="name" value="">
+    </div>
+   </div>
+   <div class="row mb-3">
+    <label class="col-sm-3 col-form-label">created</label>
+     <div class="col-sm-6">
+      <input type="text" class="form-content" name="name" value="">
+     </div>
+   </div>
+  </form>
+ </div>
 </body>
 
 </html>
