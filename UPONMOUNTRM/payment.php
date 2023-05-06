3b871bf3cc4aca2c9bb4f6513a4204b690cb79fc
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-06 22:46:04 +0100

    great

--- UPONMOUNTRM/payment.php
+++ UPONMOUNTRM/payment.php
@@ -4,6 +4,7 @@
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="form.css">
  <title>paystack</title>
 </head>
 
@@ -11,9 +12,14 @@
  <div style="background-color:blue; margin-left:500px;margin-top: 300px ">
   <form id="paymentForm">
 
-   <div class="form-group">
+   <!-- <div class="form-group">
     <label for="first-name">First Name</label>
     <input type="text" id="first-name" />
+   </div> -->
+   <div class="box">
+    <span class="details">First Name</span>
+    <input type="text" placeholder="Enter Your First Name" name="First Name">
+
    </div>
    <div class="form-group">
     <label for="last-name">Last Name</label>
@@ -27,41 +33,55 @@
     <label for="amount">Amount</label>
     <input type="tel" id="amount" required />
    </div>
+   <div class="container">
+    <div class="header">Registration</div>
+    <form action="login.html" method="GET">
+     <div class="details">
+      <div class="box">
+       <span class="details">First Name</span>
+       <input type="text" placeholder="Enter Your First Name" name="First Name">
 
-  </form>
- </div>
+      </div>
+      <div class="box">
+       <span class="details">Last Name</span>
+       <input type="text" placeholder="Enter Your Last Name" name="Last Name">
+      </div>
 
- <script src="https://js.paystack.co/v1/inline.js"></script>
 
- <script>
-  function payWithPaystack() {
-   var handler = PaystackPop.setup({
-    key: 'pk_test_eb064e623b2d625c58884613fff1b7a40c273846',
-    email: document.getElementById("email-address").value,
-    amount: document.getElementById("amount").value * 100,
-    ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-    metadata: {
-     custom_fields: [{
-      display_name: "Mobile Number",
-      variable_name: "mobile_number",
-      value: "+2348012345678"
-     }]
-    },
-    callback: function(response) {
-     alert('success. transaction ref is ' + response.reference);
-    },
-    onClose: function() {
-     alert('window closed');
+    </form>
+   </div>
+
+   <script src="https://js.paystack.co/v1/inline.js"></script>
+
+   <script>
+    function payWithPaystack() {
+     var handler = PaystackPop.setup({
+      key: 'pk_test_eb064e623b2d625c58884613fff1b7a40c273846',
+      email: document.getElementById("email-address").value,
+      amount: document.getElementById("amount").value * 100,
+      ref: '' + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+      metadata: {
+       custom_fields: [{
+        display_name: "Mobile Number",
+        variable_name: "mobile_number",
+        value: "+2348012345678"
+       }]
+      },
+      callback: function(response) {
+       alert('success. transaction ref is ' + response.reference);
+      },
+      onClose: function() {
+       alert('window closed');
+      }
+     });
+     handler.openIframe();
     }
-   });
-   handler.openIframe();
-  }
- </script>
- <div class="form-submit" style="margin-left:500px">
-  <button type="submit" onclick="payWithPaystack()"> Pay </button>
- </div>
+   </script>
+   <div class="form-submit" style="margin-left:500px">
+    <button type="submit" onclick="payWithPaystack()"> Pay </button>
+   </div>
 
- <script src="https://js.paystack.co/v1/inline.js"></script>
+   <script src="https://js.paystack.co/v1/inline.js"></script>
 
 </body>
 
