0b3ea597eb8875725ef2bb71cb912ae0a157e4e0
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-06 19:12:09 +0100

    check

--- UPONMOUNTRM/booking.php
+++ UPONMOUNTRM/booking.php
@@ -175,8 +175,7 @@
                     <div class="testimonial-item bg-transparent border rounded p-4">
                         <i class="fa fa-quote-left fa-2x text-primary mb-3"></i>
                         <p>
-                            Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor
-                            stet amet eirmod eos labore diam
+                            
                         </p>
                         <div class="d-flex align-items-center">
                             <img class="img-fluid flex-shrink-0 rounded-circle" src="img/testimonial-2.jpg" style="width: 50px; height: 50px" />
