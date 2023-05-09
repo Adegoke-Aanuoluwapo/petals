ab5a5a5526872e4e95e77f354fda9c2c9c9afd3e
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-09 10:58:56 +0100

    cont'd

--- form/myshop/OOP.php
+++ form/myshop/OOP.php
@@ -3,9 +3,18 @@
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Document</title>
+ <title>OOP</title>
 </head>
 <body>
+ <?php
+ class BankAccount{
+  public $accountNumber;
+  public $balance;
+  public function deposit($amount){
+   
+  }
+ }
  
+ ?>
 </body>
 </html>
