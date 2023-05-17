d85fb7eb6e0fc3166f8a2c67a12f84ff4d6fe760
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-17 13:49:20 +0100

    add form

--- crud/user.php
+++ crud/user.php
@@ -7,24 +7,60 @@
  <meta name="viewport" content="width=device-width, initial-scale=1">
 
  <!-- Bootstrap CSS -->
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
- <title>Hello, world!</title>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+ <title>crud operation</title>
 </head>
 
 <body>
- <h1>Hello, world!</h1>
+ <div class="container">
+  <form method="post">
+   <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Full Names</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+   </div>
+   <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Phone</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="phone">
+   </div>
+   <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Email</label>
+    <input type="email" class="form-control" id="exampleInputPassword1" name="email">
+    <div class="mb-3">
+     <label for="exampleInputPassword1" class="form-label">Sex</label>
+     <select name="position" id="">Sex</select>
+     <select name="position" id="" value="male">Male</select>
+     <select name="position" id="" value="female">Female</select>
+     <input type="password" class="form-control" id="exampleInputPassword1" name="position">
+    </div>
+    <div class="mb-3">
+     <label for="exampleInputPassword1" class="form-label">Position</label>
+     <select name="position" id="">position</select>
+     <select name="position" id="" value="member">member</select>
+     <select name="position" id="" value="worker">worker</select>
+     <input type="password" class="form-control" id="exampleInputPassword1" name="position">
+    </div>
+    <div class="mb-3">
+     <label for="exampleInputPassword1" class="form-label">State</label>
+     <select name="position" id="">state</select>
+     <select name="position" id="" value="abuja">Abuja</select>
+     <select name="position" id="" value="lagos">Lagos</select>
+     <select name="position" id="" value="ondo">Ondo</select>
+     <input type="password" class="form-control" id="exampleInputPassword1" name="position">
+    </div>
 
- <!-- Optional JavaScript; choose one of the two! -->
+   </div>
+   <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+   </div>
+   <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+ </div>
 
- <!-- Option 1: Bootstrap Bundle with Popper -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
- <!-- Option 2: Separate Popper and Bootstrap JS -->
- <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
 </body>
 
 </html>
