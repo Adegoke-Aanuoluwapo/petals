5c748e606d7866429b9141a24f6cab41e58263ab
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-18 10:53:18 +0100

    progress

--- schoolms/production/nav.php
+++ schoolms/production/nav.php
@@ -1,131 +1,132 @@
 <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Welcome,</span>
-                <h2>Mega Mind</h2>
-              </div>
-            </div>
-            <!-- /menu profile quick info -->
+  <div class="profile_pic">
+    <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+  </div>
+  <div class="profile_info">
+    <span>Welcome,</span>
+    <h2>Mega Mind</h2>
+  </div>
+</div>
+<!-- /menu profile quick info -->
 
-            <br />
+<br />
 
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section">
-                <h3>General</h3>
-                <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="index.php">Dashboard</a></li>
-                      <li><a href="registerstud.php">Register Students</a></li>
+<!-- sidebar menu -->
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+  <div class="menu_section">
+    <h3>General</h3>
+    <ul class="nav side-menu">
+      <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
+        <ul class="nav child_menu">
+          <li><a href="index.php">Dashboard</a></li>
+          <li><a href="registerstud.php">Register Students</a></li>
+          <li><a href="registerpar.php">Register Parents</a></li>
 
-                    </ul>
-                  </li>
-                 
-                </ul>
-              </div>
-              
+        </ul>
+      </li>
 
-            </div>
-            <!-- /sidebar menu -->
+    </ul>
+  </div>
 
-            <!-- /menu footer buttons -->
-        
-            <!-- /menu footer buttons -->
+
+</div>
+<!-- /sidebar menu -->
+
+<!-- /menu footer buttons -->
+
+<!-- /menu footer buttons -->
+</div>
+</div>
+
+<!-- top navigation -->
+<div class="top_nav">
+  <div class="nav_menu">
+    <div class="nav toggle">
+      <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+    </div>
+    <nav class="nav navbar-nav">
+      <ul class=" navbar-right">
+        <li class="nav-item dropdown open" style="padding-left: 15px;">
+          <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
+            <img src="images/img.jpg" alt="">John Doe
+          </a>
+          <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="javascript:;"> Profile</a>
+            <a class="dropdown-item" href="javascript:;">
+              <span class="badge bg-red pull-right">50%</span>
+              <span>Settings</span>
+            </a>
+            <a class="dropdown-item" href="javascript:;">Help</a>
+            <a class="dropdown-item" href="signup.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
           </div>
-        </div>
+        </li>
 
-        <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+        <li role="presentation" class="nav-item dropdown open">
+          <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
+            <i class="fa fa-envelope-o"></i>
+            <span class="badge bg-green">6</span>
+          </a>
+          <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
+            <li class="nav-item">
+              <a class="dropdown-item">
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="dropdown-item">
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="dropdown-item">
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="dropdown-item">
+                <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
+                <span>
+                  <span>John Smith</span>
+                  <span class="time">3 mins ago</span>
+                </span>
+                <span class="message">
+                  Film festivals used to be do-or-die moments for movie makers. They were where...
+                </span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <div class="text-center">
+                <a class="dropdown-item">
+                  <strong>See All Alerts</strong>
+                  <i class="fa fa-angle-right"></i>
+                </a>
               </div>
-              <nav class="nav navbar-nav">
-              <ul class=" navbar-right">
-                <li class="nav-item dropdown open" style="padding-left: 15px;">
-                  <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                    <img src="images/img.jpg" alt="">John Doe
-                  </a>
-                  <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item"  href="javascript:;"> Profile</a>
-                      <a class="dropdown-item"  href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                  <a class="dropdown-item"  href="javascript:;">Help</a>
-                    <a class="dropdown-item"  href="signup.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
-                  </div>
-                </li>
-
-                <li role="presentation" class="nav-item dropdown open">
-                  <a href="javascript:;" class="dropdown-toggle info-number" id="navbarDropdown1" data-toggle="dropdown" aria-expanded="false">
-                    <i class="fa fa-envelope-o"></i>
-                    <span class="badge bg-green">6</span>
-                  </a>
-                  <ul class="dropdown-menu list-unstyled msg_list" role="menu" aria-labelledby="navbarDropdown1">
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="dropdown-item">
-                        <span class="image"><img src="images/img.jpg" alt="Profile Image" /></span>
-                        <span>
-                          <span>John Smith</span>
-                          <span class="time">3 mins ago</span>
-                        </span>
-                        <span class="message">
-                          Film festivals used to be do-or-die moments for movie makers. They were where...
-                        </span>
-                      </a>
-                    </li>
-                    <li class="nav-item">
-                      <div class="text-center">
-                        <a class="dropdown-item">
-                          <strong>See All Alerts</strong>
-                          <i class="fa fa-angle-right"></i>
-                        </a>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-        <!-- /top navigation -->
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<!-- /top navigation -->
---
+++ schoolms/production/registerpar.php
@@ -0,0 +1,216 @@
+<?php
+session_start();
+include("connection.php");
+include("function.php");
+
+if (isset($_POST['registerstudent'])) {
+ regStudent();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+ <!-- Meta, title, CSS, favicons, etc. -->
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <title>Gentelella Alela! | </title>
+
+ <?php include("header.php"); ?>
+</head>
+
+<body class="nav-md">
+ <div class="container body">
+  <div class="main_container">
+   <div class="col-md-3 left_col">
+    <div class="left_col scroll-view">
+     <div class="navbar nav_title" style="border: 0;">
+      <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+     </div>
+
+     <div class="clearfix"></div>
+
+     <?php include("nav.php"); ?>
+
+     <!-- page content -->
+     <div class="right_col" role="main">
+      <div class="">
+       <div class="page-title">
+        <div class="title_left">
+         <h3>Form Elements</h3>
+        </div>
+
+        <div class="title_right">
+         <div class="col-md-5 col-sm-5  form-group pull-right top_search">
+          <div class="input-group">
+           <input type="text" class="form-control" placeholder="Search for...">
+           <span class="input-group-btn">
+            <button class="btn btn-default" type="button">Go!</button>
+           </span>
+          </div>
+         </div>
+        </div>
+       </div>
+       <div class="clearfix"></div>
+       <div class="row">
+        <div class="col-md-12 col-sm-12 ">
+         <div class="x_panel">
+          <div class="x_title">
+           <h2>Student <small>Register Student</small></h2>
+           <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+
+
+
+           <br />
+           <form method="POST" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+
+            <div class="item form-group">
+             <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Surname <span class="required">*</span>
+             </label>
+             <div class="col-md-6 col-sm-6 ">
+              <input type="text" id="first-name" name="surname" required="required" class="form-control ">
+             </div>
+            </div>
+            <div class="item form-group">
+             <label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">First Name <span class="required">*</span>
+             </label>
+             <div class="col-md-6 col-sm-6 ">
+              <input type="text" id="last-name" name="firstname" required="required" class="form-control">
+             </div>
+            </div>
+            <div class="item form-group">
+             <label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">registration Number</label>
+             <div class="col-md-6 col-sm-6 ">
+              <input id="middle-name" class="form-control" type="text" name="reg">
+             </div>
+            </div>
+            <div class="item form-group">
+             <label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">class</label>
+             <div class="col-md-6 col-sm-6 ">
+              <select class="form-control" name="class">
+               <option>Class</option>
+               <option>JSS 1</option>
+               <option>JSS 2</option>
+              </select>
+             </div>
+            </div>
+            <div class="item form-group">
+             <label class="col-form-label col-md-3 col-sm-3 label-align">Sex</label>
+             <div class="col-md-6 col-sm-6 ">
+              <select class="form-control" name="sex">
+               <option>Gender</option>
+               <option>Male</option>
+               <option>Female</option>
+              </select>
+             </div>
+            </div>
+            <div class="item form-group">
+             <label class="col-form-label col-md-3 col-sm-3 label-align">Date Of Birth <span class="required">*</span>
+             </label>
+             <div class="col-md-6 col-sm-6 ">
+              <input id="birthday" class="date-picker form-control" name="dob" placeholder="dd-mm-yyyy" type="text" required="required" type="text" onfocus="this.type='date'" onmouseover="this.type='date'" onclick="this.type='date'" onblur="this.type='text'" onmouseout="timeFunctionLong(this)">
+              <script>
+               function timeFunctionLong(input) {
+                setTimeout(function() {
+                 input.type = 'text';
+                }, 60000);
+               }
+              </script>
+             </div>
+            </div>
+            <div class="ln_solid"></div>
+            <div class="item form-group">
+             <div class="col-md-6 col-sm-6 offset-md-3">
+              <button type="submit" name="registerstudent" class="btn btn-success">Submit</button>
+             </div>
+            </div>
+
+           </form>
+
+           <table class="table">
+            <thead>
+             <tr>
+              <th>sn</th>
+              <th>surname</th>
+              <th>firstname</th>
+              <th>Register No</th>
+              <th>class</th>
+              <th>Dates of Birth</th>
+             </tr>
+            </thead>
+            <tbody>
+             <?php $i = 1;
+             $sql = $con->query("SELECT * FROM student");
+             while ($rows = $sql->fetch_assoc()) {
+             ?>
+
+              <th scope="row"><?= $i++ ?></th>
+              <td><?= $rows['surname'] ?></td>
+              <td><?= $rows['firstname'] ?></td>
+              <td><?= $rows['reg'] ?></td>
+              <td><?= $rows['class'] ?></td>
+              <td><?= $rows['dob'] ?></td>
+              </tr>
+             <?php } ?>
+            </tbody>
+           </table>
+          </div>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+     <!-- /page content -->
+
+     <!-- footer content -->
+     <?php include("footer.php"); ?>
+     <!-- /footer content -->
+    </div>
+   </div>
+
+   <!-- jQuery -->
+   <script src="../vendors/jquery/dist/jquery.min.js"></script>
+   <!-- Bootstrap -->
+   <script src="../vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+   <!-- FastClick -->
+   <script src="../vendors/fastclick/lib/fastclick.js"></script>
+   <!-- NProgress -->
+   <script src="../vendors/nprogress/nprogress.js"></script>
+   <!-- bootstrap-progressbar -->
+   <script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+   <!-- iCheck -->
+   <script src="../vendors/iCheck/icheck.min.js"></script>
+   <!-- bootstrap-daterangepicker -->
+   <script src="../vendors/moment/min/moment.min.js"></script>
+   <script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+   <!-- bootstrap-wysiwyg -->
+   <script src="../vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js"></script>
+   <script src="../vendors/jquery.hotkeys/jquery.hotkeys.js"></script>
+   <script src="../vendors/google-code-prettify/src/prettify.js"></script>
+   <!-- jQuery Tags Input -->
+   <script src="../vendors/jquery.tagsinput/src/jquery.tagsinput.js"></script>
+   <!-- Switchery -->
+   <script src="../vendors/switchery/dist/switchery.min.js"></script>
+   <!-- Select2 -->
+   <script src="../vendors/select2/dist/js/select2.full.min.js"></script>
+   <!-- Parsley -->
+   <script src="../vendors/parsleyjs/dist/parsley.min.js"></script>
+   <!-- Autosize -->
+   <script src="../vendors/autosize/dist/autosize.min.js"></script>
+   <!-- jQuery autocomplete -->
+   <script src="../vendors/devbridge-autocomplete/dist/jquery.autocomplete.min.js"></script>
+   <!-- starrr -->
+   <script src="../vendors/starrr/dist/starrr.js"></script>
+   <!-- Custom Theme Scripts -->
+   <script src="../build/js/custom.min.js"></script>
+
+</body>
+
+</html>
