b65f763675f35f3421f0d0e900a2333f4ca51e64
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-05-19 08:47:35 +0100

    scope

--- crud/display.php
+++ crud/display.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'connect.php'
 ?>
 
@@ -7,57 +7,60 @@ include 'connect.php'
 <html lang="en">
 
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>crud operation</title>
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>crud operation</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
- <div class="container ">
-  <button class="btn btn-primary my-5"><a href="user.php" class="text-light">Add user</a></button>
- </div>
- <table class="table table-striped table-hover">
-  <thead>
-   <tr>
-    <th scope="col">sn</th>
-    <th scope="col">Full Names</th>
-    <th scope="col">Phone</th>
-    <th scope="col">Email</th>
-    <th scope="col">Sex</th>
-    <th scope="col">Status</th>
-    <th scope="col">State</th>
-    <th scope="col">Operation</th>
-   </tr>
-  </thead>
-  <tbody>
-<?php 
-$sql = "SELECT * FROM members ";
-$result = mysqli_query($con, $sql);
-if($result){
- 
- while($row = mysqli_fetch_assoc($result)){
-   $id = $row['id'];
-   $fullname=$row['fullname'];
-   $phone=$row['phone'];
-   $email=$row['email'];
-   $sex=$row['sex'];
-   $status=$row['status'];
-   $state=$row['state'];
-   echo '<tr>
-   <th scope="row">'.$id.'</th>
-   <td>'.$fullname.'</td>
+  <div class="container ">
+    <button class="btn btn-primary my-5"><a href="user.php" class="text-light">Add user</a></button>
+  </div>
+  <table class="table table-striped table-hover">
+    <thead>
+      <tr>
+        <th scope="col">sn</th>
+        <th scope="col">Full Names</th>
+        <th scope="col">Phone</th>
+        <th scope="col">Email</th>
+        <th scope="col">Sex</th>
+        <th scope="col">Status</th>
+        <th scope="col">State</th>
+        <th scope="col">Operation</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $sql = "SELECT * FROM members ";
+      $result = mysqli_query($con, $sql);
+      if ($result) {
+
+        while ($row = mysqli_fetch_assoc($result)) {
+          $id = $row['id'];
+          $fullname = $row['fullname'];
+          $phone = $row['phone'];
+          $email = $row['email'];
+          $sex = $row['sex'];
+          $status = $row['status'];
+          $state = $row['state'];
+          echo '<tr>
+   <th scope="row">' . $id . '</th>
+   <td>' . $fullname . '</td>
+    <td>' . $phone . '</td>
+     <td>' . $email . '</td>
+      <td>' . $sex . '</td>
+         <td>' . $status . '</td>
+            <td>' . $state . '</td>
    </tr>';
-
- }
-
-}
+        }
+      }
 
 
-?>
+      ?>
 
 
-   <!-- <tr>
+      <!-- <tr>
     <th scope="row">1</th>
     <td>Mark</td>
     <td>Otto</td>
@@ -86,8 +89,8 @@ if($result){
     <td colspan="2">Larry the Bird</td>
     <td>@twitter</td>
    </tr> -->
-  </tbody>
- </table>
+    </tbody>
+  </table>
 </body>
 
 </html>
