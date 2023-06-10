e7107ca838f184831ef507e68d5cffaff905b2fa
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-06-10 21:12:29 +0100

    complete

--- schoolms/production/borrow.php
+++ schoolms/production/borrow.php
@@ -13,7 +13,7 @@ if (isset($_POST['editborrow'])) {
 }
 if(isset($_POST['delete'])){
   $id = $_POST['delete'];
-  $sql = $con->query("DELETE FROM borrow")
+  $sql = $con->query("DELETE FROM borrow WHERE student")
 
 }
 
