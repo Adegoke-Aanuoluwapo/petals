ff67b621c0a95e5ef722e0b997d2b7e0159694cb
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-06-16 10:33:57 +0100

    more

---
+++ php/fear.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Document</title>
+</head>
+<body>
+ <form method="POST">
+  <label for="name">Name 
+ </label>
+ <input type="text" name="name"><br>
+ <label for="name">Code 
+ </label>
+ <input type="text" name="code"><br>
+ <input type="submit" name="submit">
+ </form>
+ 
+</body>
+</html>
