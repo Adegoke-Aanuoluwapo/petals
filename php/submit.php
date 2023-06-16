2547798b1b860b0f484a9cee03651ffa9e9e4902
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-06-16 10:31:01 +0100

    great

--- php/submit.php
+++ php/submit.php
@@ -7,6 +7,21 @@ $dbname = "grit";
 if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
     die("failed to connect");
 }
+
+if(isset($_POST['submit'])){
+ $name =$_POST['name'];
+ $code =$_POST['code'];
+
+ $sql = "INSERT INTO people(name, code) VALUES('$name', '$code')";
+ $result = mysqli_query($con , $sql);
+ if($result){
+  echo "data inserted successfully";
+ }
+ else{
+  die(mysqli_error($con));
+ }
+
+}
 ?>
 
 
@@ -23,6 +38,15 @@ if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)){
  <title>Document</title>
 </head>
 <body>
- <?php   ?>
+ <form method="POST">
+  <label for="name">Name 
+ </label>
+ <input type="text" name="name"><br>
+ <label for="name">Code 
+ </label>
+ <input type="text" name="code"><br>
+ <input type="submit" name="submit">
+ </form>
+ 
 </body>
 </html>
