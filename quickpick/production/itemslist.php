0f3efa61f43325da0bbf35621b069572c083c036
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-06-23 10:26:13 +0100

    add more

--- quickpick/production/itemslist.php
+++ quickpick/production/itemslist.php
@@ -1,10 +1,10 @@
 <?php 
 include("functions.php");
 
-if(isset($_POST["deleteCategory"])){
+if(isset($_POST["deleteItems"])){
 	global $con;
-	$sn = $_POST['deleteCategory'];
-	$sql = $con->query("DELETE FROM category WHERE sn = '$sn' ")or die($con->error);
+	$sn = $_POST['deleteItems'];
+	$sql = $con->query("DELETE FROM items WHERE sn = '$sn' ")or die($con->error);
 
 }
 
@@ -36,7 +36,105 @@ if(isset($_POST["deleteCategory"])){
           <?php include("nav.php") ?>
 <!-- nav end-->
         <!-- page content -->
-        <div class="right_col" role="main">
+        
+
+
+			<?php if(isset($_POST['updateitems'])){
+					$sn = $_POST['updateitems'];
+					$sql = $con->query("SELECT * FROM items WHERE sn ='$sn' ");
+					$rows = mysqli_fetch_assoc($sql); 	
+			?>
+
+<div class="right_col" role="main">
+				<div class="">
+					<div class="page-title">
+						<div class="title_left">
+							<h3>Form Elements</h3>
+						</div>
+
+						<div class="title_right">
+							<div class="col-md-5 col-sm-5  form-group pull-right top_search">
+								<div class="input-group">
+									<input type="text" class="form-control" placeholder="Search for...">
+									<span class="input-group-btn">
+										<button class="btn btn-default" type="button">Go!</button>
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+					<div class="row">
+						<div class="col-md-12 col-sm-12 ">
+							<div class="x_panel">
+								<div class="x_title">
+									<h2>Item list <small>varieties of items</small></h2>
+									<ul class="nav navbar-right panel_toolbox">
+										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+										</li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+											<ul class="dropdown-menu" role="menu">
+												<li><a class="dropdown-item" href="#">Settings 1</a>
+												</li>
+												<li><a class="dropdown-item" href="#">Settings 2</a>
+												</li>
+											</ul>
+										</li>
+										<li><a class="close-link"><i class="fa fa-close"></i></a>
+										</li>
+									</ul>
+									<div class="clearfix"></div>
+								</div>
+								<div class="x_content">
+									<br />
+									<form id="demo-form2"method="POST" data-parsley-validate class="form-horizontal form-label-left">
+
+                  <div class="item form-group">
+											<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">CatID <span class="required">*</span>
+											</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input type="text"  id="first-name" required="required" class="form-control " value="<?= $rows['cat_id']?>" >
+											</div>
+										</div>
+										<div class="item form-group">
+											<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Title <span class="required">*</span>
+											</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input type="text" name="title" id="first-name" required="required" class="form-control " value="<?= $rows['title']?>" >
+											</div>
+										</div>
+										<div class="item form-group">
+											<label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Note <span class="required">*</span>
+											</label>
+											<div class="col-md-6 col-sm-6 ">
+												<input type="text" id="last-name" name="note" required="required" class="form-control" value="<?= $rows['note']?>" >
+											</div>
+										</div>
+									
+										
+									
+										<div class="ln_solid"></div>
+										<div class="item form-group">
+											<div class="col-md-6 col-sm-6 offset-md-3">
+											
+												<button type="submit" name="addcategory" class="btn btn-success" >Submit</button>
+											</div>
+										</div>
+
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+
+
+
+		<?php	} else {?>
+			
+
+
+<div class="right_col" role="main">
           <div class="">
             <div class="page-title">
               <div class="title_left">
@@ -58,29 +156,10 @@ if(isset($_POST["deleteCategory"])){
             <div class="clearfix"></div>
 
             <div class="row" style="display: block;">
-              <div class="col-md-6 col-sm-6  ">
-                <div class="x_panel">
-                  <div class="x_title">
-                   
-                     
-                    
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
-                  <div class="x_content">
+              
 
 
-                  </div>
-                </div>
-              </div>
-
-
-
-              <div class="clearfix"></div>
-
-
-              <div class="clearfix"></div>
+            
 
               <div class="col-md-12 col-sm-12  ">
                 <div class="x_panel">
@@ -134,8 +213,8 @@ if(isset($_POST["deleteCategory"])){
                             </td>
                             <td class="a-center "><?= $rows['note']?></td>
                             <form method="POST">
-                             <td class=" "><button class="btn btn-success">UPDATE</button></td>
-                             <td class=" "><button type="submit" class="btn btn-danger" name="deleteCategory" value="<?= $rows['sn']?>">DELETE</button></td>
+                             <td class=" "><button class="btn btn-success" name="updateitems">UPDATE</button></td>
+                             <td class=" "><button type="submit" class="btn btn-danger" name="deleteItems" value="<?= $rows['sn']?>">DELETE</button></td>
                             </form>
                             
                           </tr>
@@ -151,6 +230,7 @@ if(isset($_POST["deleteCategory"])){
                 </div>
               </div>
             </div>
+                      <?php  } ?>
           </div>
         </div>
         <!-- /page content -->
