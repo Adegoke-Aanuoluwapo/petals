9b73d9da36ed1217fd8ef710e3b0fd36e45ce001
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-06-30 13:06:44 +0100

    gritiful

--- food-order/admin/index.php
+++ food-order/admin/index.php
@@ -9,20 +9,53 @@
  </header>
  <body>
   <!-- Menu Section Starts -->
-  <div class="menu">
+  <div class="menu text-center">
    <div class="wrapper">
-
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Food</a></li>
+        <li><a href="#">Order</a></li>
+    </ul>
    </div>
   </div>
    <!-- Menu Section Ends -->
     <!-- Main content Starts -->
-  <div class="main-container">
+  <div class="main-content">
+    <div class="wrapper">
+      <h1>DASHBOARD</h1>
+      <div class="col-4 text-center">
+        <h1>5</h1>
+        <br>
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h1>5</h1>
+        <br>
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h1>5</h1>
+        <br>
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h1>5</h1>
+        <br>
+        Category
+      </div>
+      
+      <div class="clearfix"></div> 
+    </div>
    
   </div>
    <!-- Main content Ends -->
     <!-- Footer content Starts -->
   <div class="footer">
-   
+   <div class="wrapper">
+    <p class="text-center">2023 All right Reserved, Some restaurant. Developed by -<a href="#">Immaculate</a></p>
+   </div>
   </div>
    <!-- Footer content Ends -->
  </body>
