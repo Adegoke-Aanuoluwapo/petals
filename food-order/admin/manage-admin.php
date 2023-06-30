1350a5b3961704384260820130a26558eb5da8d3
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-06-30 13:09:45 +0100

    great

---
+++ food-order/admin/manage-admin.php
@@ -0,0 +1,43 @@
+<?php
+ echo "Hello Admin"
+?>
+
+<html>
+ <header>
+  <title>Food Order Website</title>
+<link rel="stylesheet" href="../css/admin.css">
+ </header>
+ <body>
+  <!-- Menu Section Starts -->
+  <div class="menu text-center">
+   <div class="wrapper">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Food</a></li>
+        <li><a href="#">Order</a></li>
+    </ul>
+   </div>
+  </div>
+   <!-- Menu Section Ends -->
+    <!-- Main content Starts -->
+  <div class="main-content">
+    <div class="wrapper">
+      <h1>DASHBOARD</h1>
+      
+      
+      <div class="clearfix"></div> 
+    </div>
+   
+  </div>
+   <!-- Main content Ends -->
+    <!-- Footer content Starts -->
+  <div class="footer">
+   <div class="wrapper">
+    <p class="text-center">2023 All right Reserved, Some restaurant. Developed by -<a href="#">Immaculate</a></p>
+   </div>
+  </div>
+   <!-- Footer content Ends -->
+ </body>
+</html>
