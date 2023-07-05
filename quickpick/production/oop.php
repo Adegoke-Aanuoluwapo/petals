42d7c80e34296264531113d2feec852011804dbd
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-05 10:30:37 +0100

    more

--- quickpick/production/oop.php
+++ quickpick/production/oop.php
@@ -1,16 +1,18 @@
 <?php
-  function inverse($x){
-   if(!$x){
-    throw new Exception('Division by zero');
-   }
-   return 1/$x;
-  } 
-  try{
-   echo inverse(5);
-   echo inverse(0);
-  }catch(Exception $e){
-   echo 'Caught Exception', $e->getMessage(), ' ';
-  }
+
+
+class User{
+  //Properties are attributes that belong to a class
+  public $name;
+  public $email;
+  public $password;
+}
+
+//instatiate
+$user1 = new User();
+$user1->name = "Gbemiga";
+
+echo $user1->name;
 ?>
 
 <!DOCTYPE html>
@@ -24,11 +26,68 @@
     <title>
      javascsript
     </title>
+    <style>
+      .container{
+        display: grid;
+        background-color: aliceblue; 
+       
+        grid-template-areas: 'a  b'
+                              'd c';
+        
+      }
+      .a{
+        background-color: red;
+        border-color: 1px solid grey;
+        grid:a;
+        
+        border-top-left-radius: 100%;
+         height:100px;
+        width: 100px;
+        padding: 285px;
+        padding-right: -25%;
+        
+      }
+      .b{
+        background-color: blue;
+        border-color: 1px solid grey;
+         border-top-right-radius: 100%;
+        height:100px;
+        width: 100px;
+        padding: 285px;
+        grid: b;
+      }
+      .c{
+        background-color: black;
+        border-color: 1px solid grey;
+        
+         height:100px;
+        width: 100px;
+        padding: 285px;
+        grid: c;
+        border-bottom-left-radius: 100%;
+      }
+      .d{
+        background-color: purple;
+        border-color: 1px solid grey;
+        padding: 285px;
+         height:100px;
+        width: 100px;
+        grid:d;
+        border-bottom-right-radius: 100%;
+        
+      }
+    </style>
     </head>
 
 
     <body>
-
+      <div class="container">
+        <div class="a"></div>
+         <div class="b"></div>
+          <div class="c"></div>
+           <div class="d"></div>
+      </div>
+      
     </body>
 
 </html>
