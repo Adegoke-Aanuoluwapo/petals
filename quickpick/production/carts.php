809e52e41edf16b3c05091777849825472703027
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-05 14:18:57 +0100

    fifth

--- quickpick/production/carts.php
+++ quickpick/production/carts.php
@@ -50,46 +50,14 @@ $rows = $sql->fetch_assoc();
 
             <div class="clearfix"></div>
 
-            <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Welcome,</span>
-                <h2>John Doe</h2>
-              </div>
-            </div>
-            <!-- /menu profile quick info -->
+          
 
             <br />
 
-            <!-- sidebar menu -->
-        
-        <?php include("nav.php")?>
-            <!-- /sidebar menu -->
-
-            <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
-            </div>
-            <!-- /menu footer buttons -->
-          </div>
-        </div>
-
+       
+          
         <!-- top navigation -->
-  <?php include("nav.php")?>
+   <?php include("nav.php")?>
         <!-- /top navigation -->
 
         <!-- page content -->
