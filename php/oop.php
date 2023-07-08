62aec48acc5e3c20591a7de8a01a0ad9e232fec8
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-08 15:54:09 +0100

    more

--- php/oop.php
+++ php/oop.php
@@ -9,14 +9,20 @@ class User{
  function set_name($name){
   $this->name = $name;
  }
+ function get_name(){
+  return $this->name;
+ }
 }
 
 $user1 = new User();
+$user2 = new User();
 
 $user1->set_name("Brad");
+$user2->set_name("ajike");
 
 
-echo $user1->name
+echo $user1->name;
+echo $user2->name;
 
 
 ?>
