cfd7ad251706602cfa104f1471e9af3d79c3944d
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-09 18:58:59 +0100

    dedication

--- php/oop.php
+++ php/oop.php
@@ -16,13 +16,15 @@ class User{
 
 $user1 = new User();
 $user2 = new User();
+$user3 = new User();
+$user4 = new User();
 
 $user1->set_name("Brad");
 $user2->set_name("ajike");
 
+echo $user1->get_name()." <br>";
+echo $user2->get_name()
 
-echo $user1->name;
-echo $user2->name;
 
 
 ?>
