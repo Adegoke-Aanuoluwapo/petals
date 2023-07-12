3ed56e11dfaf05f83d85cd578480da7bb9381934
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-12 13:25:09 +0100

    fourth

--- quickpick/production/signin.php
+++ quickpick/production/signin.php
@@ -1,17 +1,3 @@
-<?php
-include "connect.php";
-include "functions.php";
-
-if(isset($_POST["AddUsers"])){
-	global $con;
-addUsers();
-}
-
-
-
-
-?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,39 +25,28 @@ addUsers();
 
   <body class="login">
     <div>
-    
-
-
-        <div id="register" class="animate form registration_form">
+      <div class="login_wrapper">
+        <div class="animate form login_form">
           <section class="login_content">
-            <form id="demo-form2" method="post" data-parsley-validate class="form-horizontal form-label-left">
-              <h1>Create Account</h1>
+            <form>
+              <h1>Login Form</h1>
               <div>
-               
-                <input type="text" class="form-control" placeholder="Enter your name" required="" name="name" />
-              </div>
-
-              <div>
-                <input type="email" class="form-control" placeholder="Email" required="" name="email" />
-              </div>
-
-              <div>
-                <input type="tel" class="form-control" placeholder="Phone number" required="" name="phone" />
-              </div>
-              <br>
-
-              <div>
-                <input type="password" class="form-control" placeholder="Password" required=""  name="password"/>
+                <input type="text" class="form-control" placeholder="Username" required="" />
               </div>
               <div>
-                <button type="submit" name="AddUsers" class="btn btn-success" >Submit</button>
+                <input type="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                
+                <a class="btn btn-default submit" href="SignUp.php" >Log in</a>
+                <a class="reset_pass" href="#">Lost your password?</a>
               </div>
 
               <div class="clearfix"></div>
 
               <div class="separator">
-                <p class="change_link">Already a member ?
-                  <a href="signin.php" name="login" class="to_register"> Log in </a>
+                <p class="change_link">New to site?
+                  <a href="SignUp.php" class="to_register"> Create Account </a>
                 </p>
 
                 <div class="clearfix"></div>
@@ -82,8 +57,6 @@ addUsers();
             </form>
           </section>
         </div>
-      </div>
-      
-    </div>
+            </div>
   </body>
 </html>
