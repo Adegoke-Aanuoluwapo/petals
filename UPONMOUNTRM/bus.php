db981f0523344348725e8a274e79911a11a425b0
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-15 18:00:13 +0100

    more

--- UPONMOUNTRM/bus.php
+++ UPONMOUNTRM/bus.php
@@ -19,14 +19,14 @@ include("nav.php");
               <div class="bg-2 box-square hover-up wow animate__animated animate__fadeIn" data-wow-delay=".1s">
                 <h4 class="text-heading-4 color-gray-900 mb-15">18-Seater Bus</h4>
                 <p class="text-body-text-md color-gray-600">For Evangelism Missions and conveying members to church.</p>
-                <div class="box-image-inner bg-color-1"><img src="assets/imgs/page/homepage2/hummerbus2.jpeg" alt="Agon"></div>
+                <div class="box-image-inner bg-color-1"><img src="assets/imgs/page/homepage2/bus2.jpg" alt="Agon"></div>
               </div>
             </div>
             <div class="col-lg-6 col-sm-12">
               <div class="bg-6 box-square hover-up wow animate__animated animate__fadeIn" data-wow-delay=".2s">
                 <h4 class="text-heading-4 color-gray-900 mb-15">Standby Bus</h4>
                 <p class="text-body-text-md color-gray-600">For Evangelism Missions and conveying members to church.</p>
-                <div class="box-image-inner bg-color-2"><img src="assets/imgs/page/homepage2/hummerbus.jpeg" alt="Agon"></div>
+                <div class="box-image-inner bg-color-2"><img src="assets/imgs/page/homepage2/bus3.jpg" alt="Agon"></div>
               </div>
             </div>
           </div>
