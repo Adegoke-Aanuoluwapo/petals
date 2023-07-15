2efe14c71ff7c58bb898a968dac2259b102af477
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-15 16:42:16 +0100

    progress

--- php/SavingAccount.php
+++ php/SavingAccount.php
@@ -1,5 +1,22 @@
 <?php
 
 class SavingAccount extends BankAccount{
- 
+ private $interestRate;
+ public function setInterestRate($interestRate){
+  $this->interestRate = $interestRate;
+ }
+ public function addInterest(){
+  //calculate interest
+  $interest = $this->interestRate * $this->getBalance();
+  //deposit interest to balance
+  $this->deposit($interest);
+ }
+
 }
+
+$account = new SavingAccount();
+$account->deposit(100);
+//set interest rate
+$account->setInterestRate(0.05);
+$account->addInterest();
+echo $account->getBalance();
--- php/inheritance.php
+++ php/inheritance.php
@@ -1,5 +1,6 @@
 <?php
-
+ require_once 'SavingAccount.php';
+ 
 class BankAccount{
  private $balance;
  public function getBalance(){
