ee9e63df4b05a84de80e07b63b493ae6efbe5e0a
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-16 16:35:46 +0100

    finish

--- php/Namespace/index.php
+++ php/Namespace/index.php
@@ -2,6 +2,8 @@
 
 //include("PaymentGateway/Stripe/Transaction.php");
 //include("PaymentGateway/Paddle/Transaction.php");
-//require_once './PaymentGateway/Stripe/Transaction.php';
-//require_once './PaymentGateway/Stripe/Transaction.php';
-var_dump(new PaymentGateway\Stripe\Transaction());
+require_once './PaymentGateway/Stripe/Transaction.php';
+require_once './PaymentGateway/Stripe/Transaction.php';
+
+use PaymentGateway\Stripe\Transaction;
+var_dump(new Transaction());
