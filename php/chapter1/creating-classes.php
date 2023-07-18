3b5c23e0ae7faf4685648dd641684d26ab7084e6
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-18 19:57:16 +0100

    superb

--- php/chapter1/creating-classes.php
+++ php/chapter1/creating-classes.php
@@ -4,7 +4,13 @@
 class Product
 {
     public $name = 'soap';
-    public $price = 10; 
+    public $price; 
+
+    public function priceAsCurrency()
+    {
+            $priceAsCurrency = $this->price /100;
+            return $priceAsCurrency;   
+    }
 }
 
 // $product = new Product(); 
@@ -15,6 +21,12 @@ $soapObject = new Product;
 echo($soapObject->name). '<br>';
 $soapObject->name="toothpaste";
 echo($soapObject->name). '<br>';
-echo($soapObject->price). '<br>';
-$soapObject->price= 56;
-echo($soapObject->price). '<br>';
+var_dump($soapObject). '<br>';
+$soapObject->price = 550;
+var_dump($soapObject->price);
+$priceAsCurrency = $soapObject->priceAsCurrency();
+var_dump($priceAsCurrency);
+$soapObject2 = new Product();
+$soapObject2->price = 670;
+$priceAsCurrency = $soapObject2->priceAsCurrency();
+var_dump($priceAsCurrency);
