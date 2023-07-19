ef5316ca5773a2ae2b16509afca013af2a891f49
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-19 12:30:57 +0100

    construct

--- php/chapter1/creating-classes.php
+++ php/chapter1/creating-classes.php
@@ -6,6 +6,11 @@ class Product
     public $name = 'soap';
     public $price; 
 
+    public function __construct($name, $price)
+    {
+        $this->name = $name;
+         $this->price = $price;
+    }
     public function priceAsCurrency()
     {
             $priceAsCurrency = $this->price /100;
@@ -17,16 +22,20 @@ class Product
 // var_dump($product);
 // $anotherProduct = new Product;
 // var_dump($anotherProduct);
-$soapObject = new Product;
-echo($soapObject->name). '<br>';
-$soapObject->name="toothpaste";
-echo($soapObject->name). '<br>';
-var_dump($soapObject). '<br>';
-$soapObject->price = 550;
-var_dump($soapObject->price);
-$priceAsCurrency = $soapObject->priceAsCurrency();
-var_dump($priceAsCurrency);
-$soapObject2 = new Product();
-$soapObject2->price = 670;
-$priceAsCurrency = $soapObject2->priceAsCurrency();
-var_dump($priceAsCurrency);
+// $soapObject = new Product();
+// echo($soapObject->name). '<br>';
+// $soapObject->name="toothpaste";
+// echo($soapObject->name). '<br>';
+// var_dump($soapObject). '<br>';
+// $soapObject->price = 550;
+// var_dump($soapObject->price);
+// $priceAsCurrency = $soapObject->priceAsCurrency();
+// var_dump($priceAsCurrency);
+// $soapObject2 = new Product();
+// $soapObject2->price = 670;
+// $priceAsCurrency = $soapObject2->priceAsCurrency();
+// var_dump($priceAsCurrency);
+$product = new Product('Soap', 300);
+
+print $product->name;
+print $product->price;
