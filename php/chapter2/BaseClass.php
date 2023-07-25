ff83773e8c906363bbfb605e462bd6cbf00f0fe2
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-25 23:08:26 +0100

    giv

--- php/chapter2/BaseClass.php
+++ php/chapter2/BaseClass.php
@@ -2,5 +2,8 @@
 require_once "SubClass.php";
 class BaseClass  extends SubClass
 {
-   
+   public function getParentProtectedPropert():string
+   {
+       return $this->protectedProperty;
+   }
 }
