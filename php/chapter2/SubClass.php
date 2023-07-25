c4b9dc3c1c8a9079c1487df74897f831372e68fe
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-25 22:54:42 +0100

    git great

--- php/chapter2/SubClass.php
+++ php/chapter2/SubClass.php
@@ -3,4 +3,5 @@
 class SubClass
 {
    public $publicProperty = 'method that can be accessed from anywhere in the code';
+   protected $protectedProperty = 'Protected properties and method can only be accessed within the decalaring class or from  a subclass ';
 }
