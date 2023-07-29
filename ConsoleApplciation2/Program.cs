99e04ef2fb0a51859fd8acb80b40dd62ba584718
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-07-29 16:36:35 +0100

    more

--- ConsoleApplciation2/Program.cs
+++ ConsoleApplciation2/Program.cs
@@ -1,11 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
-
+ 
 public class HelloWorld
 {
     public static void Main(string[] args)
-    {
-        Console.WriteLine ("Hello Mono World");
-    }
+    {   
+        string phrase = "Giraffe Academy";
+        //char grade = 'A';
+        //int age = 30;
+        //bool isMale = false;
+        //double gpa = 3.3;
+        Console.WriteLine(phrase.ToUpper());
+        Console.WriteLine(phrase.ToLower());
+        Console.WriteLine(phrase.Contains("Academy"));
+        Console.WriteLine(phrase[3]);
+
+ }
 }
