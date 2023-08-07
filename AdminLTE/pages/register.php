59d682ddac91a19a8a0f821d50c523369fdb0ae8
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-07 16:37:08 +0100

    great

--- AdminLTE/pages/register.php
+++ AdminLTE/pages/register.php
@@ -1,5 +1,64 @@
+<?php
+
+require_once("USER.PHP");
+
+
+class Login
+{
+
+  function __construct()
+  {
+    if (array_key_exists('login', $_POST)) {
+      $this->loginUser();
+    }
+    else if(array_key_exists('register', $_POST )){
+      $this->RegisterUser();
+    }
+  }
+  public function RegisterUser(){
+    global $con;
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $sql = $con->query("INSERT INTO users(firstname, lastname, email, password) VALUES('$firstname', '$lastname', '$email', '$password')");
+    mysqli_fetch_assoc($sql);
+    return;
+  }
+  public function loginUser()
+  {
+    global $con;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if (!empty($email) && !empty($password)) {
+      $sql = "INSERT * FROM users WHERE email = '$email' ";
+      $result = mysqli_query($con, $sql);
+
+      if ($result && mysqli_num_rows($result) == 1) {
+        $user_data = mysqli_fetch_assoc($result);
+        if ($user_data['password'] == $password) {
+          $_SESSION['user_id'] = $user_data['user_id'];
+          header("location: index.php");
+          exit;
+        }
+      }
+      echo "Wrong email and password";
+    } else {
+      echo "Please enter some valid information";
+    }
+    return;
+  }
+}
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,77 +73,78 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
 </head>
+
 <body class="hold-transition register-page">
-<div class="register-box">
-  <div class="register-logo">
-    <a href="index2.php"><b>Northbrook</b>University</a>
-  </div>
+  <div class="register-box">
+    <div class="register-logo">
+      <a href="index2.php"><b>Northbrook</b>University</a>
+    </div>
 
-  <div class="card">
-    <div class="card-body register-card-body">
-      <p class="login-box-msg">Register a new membership</p>
+    <div class="card">
+      <div class="card-body register-card-body">
+        <p class="login-box-msg">Register a new membership</p>
 
-      <form action="index.php" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="First name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+        <form  method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="First name" name="firstname">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Last name">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Last name" name="lastname">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-user"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email" name="email">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" name="password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Retype password">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="col-12">
-            <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
-              <label for="agreeTerms">
-               I agree to the <a href="#">terms</a>
-              </label>
+          <div class="row">
+            <div class="col-12">
+              <div class="icheck-primary">
+                <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                <label for="agreeTerms">
+                  I agree to the <a href="#">terms</a>
+                </label>
+              </div>
             </div>
           </div>
-        </div>    
-        <div class="row mt-3">
+          <div class="row mt-3">
             <!-- /.col -->
             <div class="col-12">
-              <button type="submit" class="btn btn-success btn-block">Register</button>
+              <button type="submit" class="btn btn-success btn-block" name="register">Register</button>
             </div>
-          <!-- /.col -->
-        </div>
-      </form>
+            <!-- /.col -->
+          </div>
+        </form>
 
-      <!-- <div class="social-auth-links text-center">
+        <!-- <div class="social-auth-links text-center">
         <p>- OR -</p>
         <a href="#" class="btn btn-block btn-primary">
           <i class="fab fa-facebook mr-2"></i>
@@ -96,18 +156,19 @@
         </a>
       </div> -->
 
-      <a href="login.php" class="text-center">I already have a membership</a>
-    </div>
-    <!-- /.form-box -->
-  </div><!-- /.card -->
-</div>
-<!-- /.register-box -->
+        <a href="login.php" class="text-center">I already have a membership</a>
+      </div>
+      <!-- /.form-box -->
+    </div><!-- /.card -->
+  </div>
+  <!-- /.register-box -->
 
-<!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="../plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
