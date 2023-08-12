4ebb072a77f7233a7bd797a3763b101c70e4e520
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-08-12 13:17:47 +0100

    gratitude

--- AdminLTE/pages/allstaff.php
+++ AdminLTE/pages/allstaff.php
@@ -4,18 +4,28 @@
 <head>
  <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1">
- <title>AdminLTE 3 | DataTables</title>
+ <title>All Staff</title>
 
  <!-- Google Font: Source Sans Pro -->
  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
  <!-- Font Awesome -->
- <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
- <!-- DataTables -->
- <link rel="stylesheet" href="../../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
- <link rel="stylesheet" href="../../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
- <link rel="stylesheet" href="../../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+ <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+ <!-- Ionicons -->
+ <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+ <!-- Tempusdominus Bootstrap 4 -->
+ <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+ <!-- iCheck -->
+ <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+ <!-- JQVMap -->
+ <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
  <!-- Theme style -->
- <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+ <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+ <!-- overlayScrollbars -->
+ <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+ <!-- Daterange picker -->
+ <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
+ <!-- summernote -->
+ <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -36,11 +46,11 @@
         <table id="example1" class="table table-bordered table-striped">
          <thead>
           <tr>
-           <th>Rendering engine</th>
-           <th>Browser</th>
-           <th>Platform(s)</th>
-           <th>Engine version</th>
-           <th>CSS grade</th>
+           <th>Name</th>
+           <th>Email</th>
+           <th>Phone</th>
+           <th>Role</th>
+           
           </tr>
          </thead>
          <tbody>
@@ -53,15 +63,7 @@
            <td> 4</td>
            <td>X</td>
           </tr>
-          <tr>
-           <td>Trident</td>
-           <td>Internet
-            Explorer 5.0
-           </td>
-           <td>Win 95+</td>
-           <td>5</td>
-           <td>C</td>
-          </tr>
+      
 
          </tbody>
          <tfoot>
