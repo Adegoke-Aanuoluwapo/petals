b89c97a29fd33a185647068f59585a13a6703e9e
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-09-12 10:42:02 +0100

    gritting

---
+++ AdminLTE/pages/addresult.php
@@ -0,0 +1,102 @@
+<?php
+require("myclass.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="csrf-token" content="cXO45Tk6wcpzfsXXv7F1QFE7Jb0Pb4rG4VoM1K0E">
+ <title> Check Result
+ </title>
+ <!-- Google Font: Source Sans Pro -->
+ <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+ <!-- Font Awesome -->
+ <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+ <!-- Ionicons -->
+ <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+ <!-- Tempusdominus Bootstrap 4 -->
+ <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+ <!-- iCheck -->
+ <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+ <!-- JQVMap -->
+ <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
+ <!-- Theme style -->
+ <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+ <!-- overlayScrollbars -->
+ <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+ <!-- Daterange picker -->
+ <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
+ <!-- summernote -->
+ <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.min.css">
+ <style>
+  .profile_pics {
+   width: 50px;
+   height: 50px;
+  }
+
+  .object-cover {
+   object-fit: cover;
+  }
+
+  .profile-user-img {
+   width: 100px;
+   height: 100px;
+  }
+ </style>
+
+
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+ <div class="wrapper">
+
+
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+   <ul class="navbar-nav">
+    <li class="nav-item">
+     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+     <a href="#" class="nav-link"><b>SchoolPetal International School</b></a>
+    </li>
+   </ul>
+
+   <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+     <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+      <i class="fas fa-search"></i>
+     </a>
+     <div class="navbar-search-block">
+      <form class="form-inline">
+       <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+         <button class="btn btn-navbar" type="submit">
+          <i class="fas fa-search"></i>
+         </button>
+         <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+          <i class="fas fa-times"></i>
+         </button>
+        </div>
+       </div>
+      </form>
+     </div>
+    </li>
+    <li class="nav-item">
+     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+      <i class="fas fa-expand-arrows-alt"></i>
+     </a>
+    </li>
+    <li class="nav-item">
+     <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+      <i class="fas fa-user-alt"></i>
+     </a>
+    </li>
+   </ul>
+  </nav>
+
+  <?php include("nav.php") ?>
