163ce9188cf2247c10a3e10102f8d58815ff3d3e
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-09-20 10:08:46 +0100

    great

--- AdminLTE/pages/myclass.php
+++ AdminLTE/pages/myclass.php
@@ -171,7 +171,8 @@ function Gradeset(){
   $cr = $_POST['cr'];
   $dr = $_POST['dr'];
   $er = $_POST['er'];
-  $con->query("UPDATE result_setup SET A = '$A', B = '$B', C = '$C', D = '$D', E = '$E', F = '$F' ");
+  $fr = $_POST['fr'];
+  $con->query("UPDATE result_setup SET A = '$A', B = '$B', C = '$C', D = '$D', E = '$E', F = '$F', ar = '$ar', br = '$br', cr = '$cr', dr ='$dr', er = '$er', fr = '$fr'  ");
 
 }
 function ResultSetup(){
