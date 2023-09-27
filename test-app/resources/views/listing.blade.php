a0558fa32e45d1d280cbb0bc4379bac2faa7cca2
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-09-27 10:26:59 +0100

    grate

--- test-app/resources/views/listing.blade.php
+++ test-app/resources/views/listing.blade.php
@@ -23,21 +23,23 @@
    </h3>
    <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
    <ul class="flex">
-    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2"><a href="#">API</li>
+    <li class="bg-black text-white rounded-xl px-3 py-1 mr-2"><a href="#">Laravel</li>
      <li class="bg-black text-white rounded-xl px-3 py-1 mr-2"><a href="#">Backend</li>
       <li class="bg-black text-white rounded-xl px-3 py-1 mr-2"><a href="#">Vue</li>
   
    </ul>
    <div class="text-lg my-4">
-    <i class="fa-solid fa-location-dot"></i>
+    <i class="fa-solid fa-location-dot"></i>{{$listing->location}}
    </div>
    <div class="border border-gray-200 w-full mb-6"></div>
    <div>
-    <h3 class="text-3xl font-bold mb-4">{{$listing->description}}</h3>
+    <h3 class="text-3xl font-bold mb-4">Descrption</h3>
     <div class="text-lg space-y-6">
-     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim tempore quam aperiam doloribus rerum alias consequuntur repellat eos optio quidem at dolores, laboriosam ad eligendi sequi, velit, ut porro impedit?
-     </p>
+     {{$listing->description}}
+     <a href="{{$listing->email}}" class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80">
+      <i class="fa-solid fa-envelope"> Contact Employer</i>
+     </a>
+     <a href="{{$listing->website}}" target="_blank" class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-globe"></i>Visit Website</a>
     </div>
    </div>
   </div>
