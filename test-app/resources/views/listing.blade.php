a5e6565130a7e11a8499be52cc7c2c564daa32ec
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-09-27 09:54:48 +0100

    gratify

--- test-app/resources/views/listing.blade.php
+++ test-app/resources/views/listing.blade.php
@@ -9,14 +9,25 @@
 @extends('layout')
 
 @section('content')
+@include('partials._search')
 
-<h2>
- {{$listing['title']}} 
-</h2>
-
-<p>
- {{$listing['description']}}
-</p>
+<a href="/" class="inline-block text-black ml-4 mb-4">
+<i class="fa-solid fa-arrow-left">Back</i>
+</a>
+<div class="mx-4">
+ <div class="bg-grey-50 border border-gray-200 p-10 rounded">
+  <div class="flex flex-col items-center justify-center text-center">
+   <img class="w-48 mr-6 mb-6" src="{{asset('images/no-image.png')}}" alt="" />
+   <h3 class="text-2xl mb-2">
+    {{$listing->title}}
+   </h3>
+   <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
+   <ul class="flex">
+    <li class="bg--black text-white rounded-xl"></li>
+   </ul>
+  </div>
+ </div>
+</div>
 {{-- @endforeach
 
     
