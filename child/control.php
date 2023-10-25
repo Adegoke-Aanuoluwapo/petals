e61494c520a716322e59e13818263904d4631055
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-10-25 20:05:55 +0100

    more

--- child/control.php
+++ child/control.php
@@ -1,9 +1,20 @@
 <?php
-$db = new mysqli('localhost', 'root', '', 'child');
+$con = new mysqli('localhost', 'root', '', 'child');
 
 class Children{
  
-
+function signIn($name, $password){
+ global $con;
+ $sql = $con->query("SELECT * FROM user WHERE name = '$name'");
+ if($sql->num_rows !=1){
+  echo "Invalid login";
+  return ;
+ }
+ $rows= $sql->fetch_assoc();
+ if(password_verify($password, $rows["password"])){
+  $_SESSION["id"] = $rows["id"];
+}
+}
 
 
 
