b212e5b7083a901dce40e2007519115af24c6332
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-11-10 11:38:05 +0100

    gratitude

--- dotnet/app/Program.cs
+++ dotnet/app/Program.cs
@@ -5,41 +5,12 @@ using System;
 namespace app
 {
    class Program{
+    static void aanuMMaculate(){
+      Console.WriteLine("I am delighted");
+    }
    static void Main(string[] args)
    {
-   Console.WriteLine("Hello, World!");
-   Console.WriteLine("Started Learning C#!");
-   Console.WriteLine(3 + 4);
-   Console.WriteLine("23*24");
-   Console.WriteLine(23*24);
-   string name = "Ajayi";
-   Console.WriteLine(name);
-   int myNumero = 444;
-   Console.WriteLine(myNumero);
-   Console.WriteLine("Hello " + name);
-   string firstName = "Johnson ";
-   string lastName = "Bright";
-   string Fullname = firstName + lastName;
-   Console.WriteLine(Fullname);
-   string message = "C#";
-   message += "string";
-   Console.WriteLine(message);
-   string nessage = "Adebimpe";
-   Console.WriteLine(nessage.Length);
-   string condition = "sunny";
-   bool free = true;
-
-
-   if(free & condition =="sunny")
-   {
-    Console.WriteLine("Let's go outside");
-   }
-   else
-   {
-    Console.WriteLine("Just stay home.");
-   }
-   string friend = "Great friend";
-   Console.WriteLine(friend);
+      aanuMMaculate();
    
    }
   }
