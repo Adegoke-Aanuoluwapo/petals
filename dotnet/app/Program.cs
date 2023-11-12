bd50b6cd522b419356d0d9ea7cb5e9fe3939d38d
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-11-12 18:20:57 +0100

    great

--- dotnet/app/Program.cs
+++ dotnet/app/Program.cs
@@ -5,13 +5,24 @@ using System;
 namespace app
 {
    class Program{
-    static void aanuMMaculate(){
-      Console.WriteLine("I am delighted");
+   
+    public int StudentId{get; set;}
+    public string Name { get; set;}
+    public string Address { get; set;}
+    public void showDetails()
+    {
+    Console.WriteLine("Student Information");
+    Console.WriteLine("StudentId: (0), Name:(1), Address(2)",StudentId, Name, Address);
     }
    static void Main(string[] args)
    {
-      aanuMMaculate();
-   
+      Student st = new Student();
+      st.StudentId =1;
+      st.Name = "Agahaowa";
+      st.Address = "Obalende CMS";
+      st.showDetails();
+      Console.ReadKey();
    }
-  }
+}
+
 }
