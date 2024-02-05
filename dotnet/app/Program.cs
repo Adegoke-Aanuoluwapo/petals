30a63ba0f5be324e946808ec5c36d9440312bf0f
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2024-02-05 15:45:54 +0100

    dive in asp.net

--- dotnet/app/Program.cs
+++ dotnet/app/Program.cs
@@ -6,22 +6,17 @@ namespace app
 {
    class Program{
    
-    public int StudentId{get; set;}
-    public string Name { get; set;}
-    public string Address { get; set;}
-    public void showDetails()
-    {
-    Console.WriteLine("Student Information");
-    Console.WriteLine("StudentId: (0), Name:(1), Address(2)",StudentId, Name, Address);
-    }
+  
+   
    static void Main(string[] args)
    {
-      Student st = new Student();
-      st.StudentId =1;
-      st.Name = "Agahaowa";
-      st.Address = "Obalende CMS";
-      st.showDetails();
-      Console.ReadKey();
+      int sum = 0;
+      for(int i =1; i<= 20; i++){
+         if (i % 3 == 0){
+            sum = sum + i;
+         }
+      }
+      Console.WriteLine($"The sum is {sum}");
    }
 }
 
---
+++ refire/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace app
+{
+ class Program
+ {
+
+
+
+  static void Main (string[] args)
+  {
+  
+  
+  //  string secretWorld = "giraffe";
+  //  string guess = "";
+  //  int guessCount = 0;
+  //  int guessLimit = 4;
+  //  bool outOfGuesses = false;
+
+  //  while(guess != secretWorld && !outOfGuesses){
+  //   if(guessCount <guessLimit){
+  //         Console.Write("Enter guess: ");
+  //         guess = Console.ReadLine()!;
+  //         guessCount++;
+  //       }
+  //   else{
+  //     outOfGuesses = true;
+  //   }
+  
+  //  }
+  //  if(outOfGuesses){
+  //   Console.Write("You Lose");
+  //  }
+  //  else{
+  //       Console.Write("You Win!");
+  //     }
+   
+
+  //  Console.ReadLine();
+
+  // }
+ //  static int GetMax(int num1, int num2, int num3)
+ //  {
+ //   // Console.Write("Enter the first number ");
+ //   // num1 = Convert.ToInt32(Console.ReadLine());
+ //   // Console.Write("Enter the second number ");
+ //   // num2 = Convert.ToInt32(Console.ReadLine());
+ //   // Console.Write("Enter the third number ");
+ //   // num3 = Convert.ToInt32(Console.ReadLine());
+
+ //   int result;
+ //   if (num1 >= num2 && num1 >= num3)
+ //   {
+ //    result = num1;
+ //   }
+ //   else if (num2 >= num1 && num2 >= num3)
+ //   {
+ //    result = num2;
+ //   }
+ //   else
+ //   {
+ //    result = num3;
+ //   }
+ //   return result;
+
+ //int i = 3;
+ for (int i = 3; i >= 12; i++ ){
+      Console.WriteLine(i);
+  
+ }
+
+   }
+  }
+
+}
